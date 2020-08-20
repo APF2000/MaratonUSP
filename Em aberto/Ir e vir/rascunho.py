c749dbcf729afa892a4bc3acb107bd9b8e0a821b
APF2000 <arthurpfonseca@gmail.com>
2020-08-20 10:40:54 -0300

    Função promissora

--- Em aberto/Ir e vir/rascunho.py
+++ Em aberto/Ir e vir/rascunho.py
@@ -1,3 +1,33 @@
+def vai_e_volta(atual, arestas, nodes_faltantes, visitados, alvo):
+
+    if(nodes_faltantes == 0 and 1 in arestas[atual]):    
+        return True
+
+    if atual in visitados:
+        index = visitados.index(atual)
+        import pdb; pdb.set_trace()
+
+        for el in visitados[index + 1:]:
+            arestas[atual].append(el)
+            if atual not in arestas[el]:
+                arestas[el].append(atual)
+
+        return False
+
+    if(len(visitados) == len(arestas)):
+        return False
+
+    visitados.append(atual)
+    for node in arestas[atual].copy():
+        import pdb; pdb.set_trace()
+        for visitado in visitados:
+            if (visitado != atual) and (atual not in arestas[visitado]):
+                arestas[visitado].append(atual)
+        if vai_e_volta(node, arestas, nodes_faltantes - 1, visitados, alvo):
+            return True
+
+    return False
+
 while True:    
     n_m = input().split()
     n = int(n_m[0])
@@ -7,11 +37,26 @@ while True:
         break
 
     ruas = []
+    arestas = {}
+    for i in range(n):
+        arestas[i + 1] = []
+    
+    #import pdb; pdb.set_trace()
     for i in range(m):
         vwp = input().split() 
+        v = int(vwp[0])
+        w = int(vwp[1])
+        p = int(vwp[2])
         
         # Se P = 1 -> Mão única (V -> W)
         # Se P = 2 -> Mão dupla (V <-> W)
-        ruas.append({'v': vwp[0], 'w': vwp[1], 'p': vwp[2]})
+        ruas.append({'v': v, 'w': w, 'p': p})
 
+        arestas[v].append(w)
+        if p == 2:
+            arestas[w].append(v)
+
+    result = vai_e_volta(1, arestas, n, [], 1)
+
+    #import pdb; pdb.set_trace()
     print(ruas)
