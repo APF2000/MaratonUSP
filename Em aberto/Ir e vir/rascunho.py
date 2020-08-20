c3710a71c009226c7fbb959909611289afe0e654
APF2000 <arthurpfonseca@gmail.com>
2020-08-20 11:10:29 -0300

    Código mais limpo

--- Em aberto/Ir e vir/rascunho.py
+++ Em aberto/Ir e vir/rascunho.py
@@ -6,33 +6,35 @@ class Solver():
 
     def vai_e_volta(self, atual, alvo):
         #import pdb; pdb.set_trace()
-        #nodes_faltantes = len(self.arestas) - len(self.visitados)
+        #nodes_faltantes = len(arestas) - len(visitados)
+        arestas = self.arestas
+        visitados = self.visitados
 
-        if(self.nodes_faltantes == 0 and 1 in self.arestas[atual]):    
+        if(self.nodes_faltantes == 0 and 1 in arestas[atual]):    
             return True
 
-        if atual in self.visitados:
-            index = self.visitados.index(atual)
+        if atual in visitados:
+            index = visitados.index(atual)
             import pdb; pdb.set_trace()
 
-            for el in self.visitados[index + 1:]:
-                if el not in self.arestas[atual]:
-                    self.arestas[atual].append(el)
-                if atual not in self.arestas[el]:
-                    self.arestas[el].append(atual)
+            for el in visitados[index + 1:]:
+                if el not in arestas[atual]:
+                    arestas[atual].append(el)
+                if atual not in arestas[el]:
+                    arestas[el].append(atual)
 
             return False
 
-        if(len(self.visitados) == len(self.arestas)):
+        if(len(visitados) == len(arestas)):
             return False
 
-        self.visitados.append(atual)
+        visitados.append(atual)
         self.nodes_faltantes -= 1
-        for node in self.arestas[atual].copy():
+        for node in arestas[atual].copy():
             import pdb; pdb.set_trace()
-            for visitado in self.visitados:
-                if (visitado != atual) and (atual not in self.arestas[visitado]):
-                    self.arestas[visitado].append(atual)
+            for visitado in visitados:
+                if (visitado != atual) and (atual not in arestas[visitado]):
+                    arestas[visitado].append(atual)
             if self.vai_e_volta(node, alvo):
                 return True
 
