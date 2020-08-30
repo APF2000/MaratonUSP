bab7c62b2c95c3b8d4b8cca072c9777e1c8f12f0
APF2000 <arthurpfonseca@gmail.com>
2020-08-30 19:39:22 -0300

    Refactor code

--- Em aberto/[1298]Corrija o/rascunho.cpp
+++ Em aberto/[1298]Corrija o/rascunho.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 #define BLOCK '*'
 #define PASS ' '
-#define FREE ' '
+#define FREE '_'
+
 
 #define HOR 'H'
 #define VERT 'V'
@@ -37,7 +38,7 @@ char** createMatrix(int n, char** bar)
     for(int i = 0; i < rows; i++)
     {
         M[i] = new char[cols];
-        cout << "Linha " << setw(2) << i << ": ";
+        cout << "Linha " << setw(2) << i << ": |";
 
         for(int j = 0; j < cols; j++)
         {
@@ -49,11 +50,6 @@ char** createMatrix(int n, char** bar)
             else if(im2 == 1 && jm2 == 1 && ijm4 == 2) M[i][j] = BLOCK;
             else
             {
-                //int row = i / 2, col = j / 4;
-                //if(i == rows - 1) row--;
-                //if(j == cols - 1) cols--;
-
-                //char conf = bar[row][col];
 
                 bool v1 = j < cols - 1 && (im4 == 0 || im4 == 2) && jm4 == 1;
                 bool v3 = i < rows - 1 && i > 1 && j < cols -1 && (im4 == 0 || im4 == 2) && jm4 == 3;
@@ -61,45 +57,23 @@ char** createMatrix(int n, char** bar)
                 bool h1 = j < cols - 2 && (jm4 == 0 || jm4 == 2) && im4 == 1;
                 bool h3 = i < rows - 1 && j > 1 && (jm4 == 0 || jm4 == 2) && im4 == 3;
 
-                //cout << "[id4=" << id4 << ", jd4=" << jd4 << "]";
-                if(i == 6){// && j == 3){
-                    //cout << "\n";
-                }
-
+                // Vertical
                 if(v1 && bar[2 * (i / 4)][(j-1) / 4] == VERT) M[i][j] = BLOCK;
                 else if(v3 && bar[2 * ((i - 2) / 4) + 1][(j-1) / 4] == VERT) M[i][j] = BLOCK;
 
+                // Horizontal
                 else if(h1 && bar[2 * (i / 4)][(j / 4)] == HOR) M[i][j] = BLOCK;
                 else if(h3 && bar[2 * (i / 4) + 1][(j-2) / 4] == HOR) M[i][j] = BLOCK;
 
-                /* Vertical
-                if(j > 0 && jm4 != 1) col = (j - 1) / 4;
-                else col = j / 4;
-
-                if(i > 0 && im4 != 1) row = (i - 1) / 4;
-                else row = i / 4;*/
-
-                /*int conf = bar[row][col];
-
-                // Vertical
-                if( (im4 == 0 || im4 == 2) && jm4 == 1 && conf == VERT) M[i][j] = BLOCK;
-                else if( (im4 == 0 || im4 == 2) && /*(i > 0 && i < rows - 1)*/ /*&& jm4 == 3 && conf == VERT) M[i][j] = BLOCK;
-
-                // Horizontal
-                else if( (jm4 == 0 || jm4 == 2) && im4 == 1 && conf == HOR) M[i][j] = BLOCK;
-                else if( (jm4 == 0 || jm4 == 2) && /*(j > 0 && j < cols - 1) &&*//* im4 == 3 && conf == HOR) M[i][j] = BLOCK;
-                */
-                // Transitions
                 else M[i][j] = PASS;
             }
 
             cout << M[i][j];//<< ", ";
         }
-        cout << "\n";
+        cout << "|\n";
     }
 
     cout << "\nFim\n";
-    //cin >> rows;
 
     return M;
 }
