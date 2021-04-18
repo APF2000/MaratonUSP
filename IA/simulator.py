cf27734dcd233a8cbbdb8899b463d89d3d95ce4c
Sergio <sergio.mcontente@gmail.com>
2021-04-18 11:48:01 -0300

    consertando o erro do simulador

--- IA/simulator.py
+++ IA/simulator.py
@@ -1,6 +1,10 @@
 #def eval_second():
 #    pass
-def change_structure(sts, cars, schedule):
+
+def score(D, fix, sts, cars, inters, schedule):
+    ##print(f'sts {sts}, \ncars {cars} \ninters {inters}')
+    #print('schedule', schedule)
+
     for st in sts.copy():
         size = sts[st]
         sts[st] = {'size': size, 'waiting': []}#set()}
@@ -10,32 +14,34 @@ def change_structure(sts, cars, schedule):
         path = cars[car]
         first = path[0]
         st_info = sts[first]#['size']
-        cars[car] = { 'path': path, 'pos': { 'name': first, 'st': st_info, 'id': st_info['size'][2] - 1 }, 'score' : 0 }
-        #print(cars[car])
+        cars[car] = { 'path': path, 'pos': { 'name': first, 'st': st_info, 'id': st_info['size'][2] - 1 } }
 
-    ####print('cars', cars)
+    ##print('cars', cars)
 
     for id in schedule.copy():
         prog = schedule[id]
         schedule[id] = {'prog': prog, 'green': 0, 'delta': 0}
 
-def score(D, fix, sts, cars, inters, schedule):
-    ###print(f'sts {sts}, \ncars {cars} \ninters {inters}')
-    ##print('schedule', schedule)
-    change_structure(sts, cars, schedule)
-    cp_cars = cars.copy()
     path = {}
     count = 0
-    for t in range(D):
+    passou_intersec = {}
+    for inter in inters.copy():
+        inters[inter] = { 'sts': inters[inter], 'passed': False }
+        #print(f'inters[{inter}] = {inters[inter]}')
+
+    for t in range(0, D):
+        for inter in inters.copy():
+            inters[inter]['passed'] = False
+
+        #breakpoint()
         aux_cars = {}
-        ##print(f'---------------------------------------------------------------------------------------------------\n schedule')
-        ##print(f'cars antes = {cars}')
-        for id in schedule:
-            pass
-            ###print(f'{id} {schedule[id]}')
+        #print(f'---------------------------------------------------------------------------------------------------\n schedule')
+        #print(f'cars antes = {cars}')
+        #if t % 1000 == 0:
+        #    for id in schedule:
+        #        #print(f'{id} {schedule[id]}')
 
         for car in cars.copy():
-            #print(f'car {car} : {cars[car]}')
 
             info = cars[car]
 
@@ -53,13 +59,19 @@ def score(D, fix, sts, cars, inters, schedule):
             end = size[1]
             id = pos['id']
 
+            if True:#car == 885 or car == 695:
+                ##print(f'schedule: {schedule}')
+                #print(f'car {car} : {cars[car]}')
+                #print(f'schedule da rua: {schedule[end]}')
+                pass
+
             # If end of path
             #breakpoint()
-            if path[-1] == st_name: #(len(path) == 0):
-                ##print(f"carro {car} CHEGOU NO DESTINO")
-                score_car = fix + (D-t)
-                count += score_car
-                cars[car]['score'] = score_car
+            # Verificar tbm se ta no fim da rua
+            if path[-1] == st_name and id >= size_st - 1: #(len(path) == 0):
+                #print(f"carro {car} CHEGOU NO DESTINO")
+                #print(f't={t}, D={D}, bonus={D-t}, fix={fix}, pountuacao antes={count}')
+                count += fix + (D - t)
                 cars.pop(car)
                 continue
 
@@ -68,25 +80,35 @@ def score(D, fix, sts, cars, inters, schedule):
             if id >= size_st - 1:
                 id_green = schedule[end]['green']
                 open_st = schedule[end]['prog'][id_green]['st']
-                ####print('openst', open_st)
+                ###print('openst', open_st)
                 wait = sts[st_name]['waiting']
-                ####print('wait', wait)
+                if car not in wait:
+                    wait.append(car)
+                    #print(f'carro {car} esperando ({wait}) na rua {st_name}')
+                ###print('wait', wait)
                 #breakpoint()
                 if open_st == st_name: # Carro passa pra proxima rua
-                    ###print('PASSANDO PRA PROXIMA RUA')
-
-                    # Será que salva? (sim)
-                    path.pop(0)
-                    n_st = path[0]
-
-                    info['pos'] = { 'name': n_st, 'st': sts[n_st], 'id': 0 }
+                    ##print('PASSANDO PRA PROXIMA RUA')
 
                     # Se tava na espera, tira, pq atravessou
-                    if car in wait:
-                        wait.remove(car)
-                else:
-                    if car not in wait:
-                        wait.append(car)#wait.add(car)
+                    if len(wait) > 0 and car == wait[0] and inters[end]['passed'] == False:#car in wait:
+
+                        # Será que salva? (sim)
+                        path.pop(0)
+                        n_st = path[0]
+
+                        info['pos'] = { 'name': n_st, 'st': sts[n_st], 'id': 0 }
+
+
+                        ##print(f'')
+                        #print(f'carro {car} saiu da espera ({wait}) na rua {st_name}; inters[{end}]={inters[end]}')
+                        #wait.remove(car)
+                        inters[end]['passed'] = True
+                        wait.pop(0)
+                # else:
+                #     if car not in wait:
+                #         wait.append(car)#wait.add(car)
+                #         #print(f'carro {car} esperando ({wait}) na rua {st_name}')
             else: # Carro anda pela rua
                 info['pos']['id'] += 1
 
@@ -95,7 +117,7 @@ def score(D, fix, sts, cars, inters, schedule):
 
         for id in schedule:
             info = schedule[id]
-            ####print('info antes', info)
+            ###print('info antes', info)
 
             prog = info['prog']
             id_green = info['green']
@@ -109,10 +131,9 @@ def score(D, fix, sts, cars, inters, schedule):
                 info['delta'] = 0
             else:
                 info['delta'] += 1
-            ####print('info depois', info)
+            ###print('info depois', info)
 
-        ##print('t: ', t)
-        ##print(f'cars = {cars}')
-    ##print("cars = ", cars)
-    cars = cp_cars
-    return count, cars
+        #print('t: ', t)
+        #print(f'cars = {cars}')
+
+    return count
