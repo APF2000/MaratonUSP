d10d1afa4da6d111cfbdebe9efefbd583dfe37c2
APF2000 <arthurpfonseca@gmail.com>
2021-05-21 16:38:15 -0300

    Problema caminho minimo

---
+++ Em aberto/ExplorerSpaces/explorer.cpp
@@ -0,0 +1,41 @@
+// https://codeforces.com/problemset/problem/1517/D
+
+#include <iostream>
+
+using namespace std;
+
+/*
+    * 1 * 1 *
+    1   1   1
+    * 1 * 1 *
+    1   1   1
+    * 1 * 1 *
+
+    2 2 4
+    1
+    3
+    4 2
+
+    * 1 *
+    4   2
+    * 3 *
+*/
+
+int main()
+{
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    for (int i = 0; i < n; i++)
+    {
+        
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        
+    }
+    
+
+    return 0;
+}
