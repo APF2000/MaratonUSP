55180f61d8e215a01c734081964084be70fb1744
APF2000 <arthurpfonseca@gmail.com>
2021-05-23 16:31:01 -0300

    Agora parece estar funcionando

--- Em aberto/ExplorerSpaces/explorer.cpp
+++ Em aberto/ExplorerSpaces/explorer.cpp
@@ -64,8 +64,11 @@ public:
     aux_x+= dir_mov[0];
     aux_y+=dir_mov[1];
     if(aux_x < m && aux_x >= 0 && aux_y < n && aux_y >= 0){
+        cout << "(" << *x << ", " << *y << ") -> (" << aux_x << ", " << aux_y << ") e valido" << endl;
+
       *x = aux_x;
       *y =  aux_y;
+
       return true;
     }
     return false;
@@ -77,15 +80,15 @@ public:
         case DOWN: return updown[x][y];
         case RIGHT: return rl[x][y];
         case LEFT: return rl[x][y - 1];
-        default: return INF;
+        default: return -INF;
     }
   }
 
   int min_path(int x, int y, int x_fut, int y_fut, int cost, int k){
-    if(k%2 == 1) return -1;
+    if(k % 2 == 1) return -1;
     if(k == 0) {
-      if (x == x_fut && y == y_fut) return cost;
-      else return INF;
+      /*if (x == x_fut && y == y_fut)*/ return cost;
+      //else return INF;
     }
     vector<int> costs = {};
     for(int dir : {UP,DOWN,LEFT,RIGHT}){
@@ -94,10 +97,12 @@ public:
       int new_cost = INF;
       if(can_move(dir, &aux_x, &aux_y)){
         int tmp_cost = calc_cost(dir,x,y);
-        if(tmp_cost != INF) tmp_cost += cost;
-        tmp_cost *= 2;
+        if(tmp_cost != INF) tmp_cost += tmp_cost + cost; // soma tmp com ele mesmo para contar ida e volta
+        //tmp_cost *= 2;
 
+        cout << "Chamando min_path(" << aux_x << ", " << aux_y << ", " << x_fut << ", " << y_fut << ", " << tmp_cost << ", " << k-2 << "){" << endl;
         new_cost = min_path(aux_x, aux_y, x_fut, y_fut, tmp_cost, k-2);
+        cout << "} (" << k << ")" << endl;
         cout << "Para a pos = (" << aux_x << ", " << aux_y << ") =>  cost antigo: " << tmp_cost << "; newcost: " << new_cost << endl;
 
       }
@@ -107,9 +112,9 @@ public:
     //int min = *(min_element(costs.begin(), costs.end()));
     sort(costs.begin(), costs.end());
     int min = costs[0];
-    cout << "Min cost: " << min << ", max: " << costs[3] << endl;
+    cout << "Min cost: " << min << endl;//", max: " << costs[3] << endl;
 
-    return min + cost;
+    return min;// + cost;
   }
 };
 int main()
