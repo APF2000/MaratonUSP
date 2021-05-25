4cad17be57a3a947e984a5ac1c217556d59c3c6a
APF2000 <arthurpfonseca@gmail.com>
2021-05-24 21:44:43 -0300

    Codigo limpo, mas ainda nao passa nos testes

--- Em aberto/ExplorerSpaces/explorer.cpp
+++ Em aberto/ExplorerSpaces/explorer.cpp
@@ -17,8 +17,7 @@
 #define R_DIR {0, 1}
 
 using namespace std;
-//map<int, vector<int> > DIR = {{UP, [0, 1]}, {DOWN, [0,-1]}, {LEFT, [-1, 0]}, {RIGHT, [1,0]}};
-//map<int, (*int) > DIR = {{UP, {0, 1}}, {DOWN, {0,-1}}, {LEFT, {-1, 0}}, {RIGHT, {1,0}}};
+
 map<int, vector< int > > DIR = {{UP, U_DIR}, {DOWN, D_DIR}, {LEFT, L_DIR}, {RIGHT, R_DIR}};
 
 
@@ -110,8 +109,6 @@ public:
     aux_x+= dir_mov[0];
     aux_y+=dir_mov[1];
     if(aux_x < m && aux_x >= 0 && aux_y < n && aux_y >= 0){
-        //cout << "(" << *x << ", " << *y << ") -> (" << aux_x << ", " << aux_y << ") e valido" << endl;
-
       *x = aux_x;
       *y =  aux_y;
 
@@ -132,16 +129,11 @@ public:
 
   int min_path(int x, int y, int k){
     if(k % 2 == 1) return -1;
-    if(k == 0) {
-      /*if (x == x_fut && y == y_fut)*/ return 0;//cost;
-      //else return INF;
-    }
-    int min_x = -1, min_y = -1; // jamais deveriam passar pelo FOR continuando com esse valor
-    int min_cost = INF;
+    if(k == 0) return 0;
     
+    int min_cost = INF;
     int aux_k = k -2;
-
-    //vector<int> costs = {};
+    
     for(int dir : {UP,DOWN,LEFT,RIGHT}){
       int aux_x = x;
       int aux_y = y;
@@ -149,66 +141,30 @@ public:
       int new_cost = INF;
 
       if(can_move(dir, &aux_x, &aux_y)){
-        
-        //for (int i = 0; i < 100; i++) cout << "-";
-        //cout << endl;
 
         int move_cost = calc_cost(dir,x,y);
         if(move_cost != INF) move_cost = (2 * move_cost);// + cost; // x2 para contar ida e volta
-        //else{ cout << "TMPCOST == INF" << endl; }
-        //move_cost *= 2;
 
         int next_cell_cost;
-        if(cost_was_calc(aux_x, aux_y, aux_k)){
-          next_cell_cost = mem_costs[aux_x][aux_y][aux_k];
-          //cout << "Valor ja existia: mem_costs[" << aux_x << "][" << aux_y << "][" << aux_k << "] == " << next_cell_cost << endl;
-        }
+        if(cost_was_calc(aux_x, aux_y, aux_k)) next_cell_cost = mem_costs[aux_x][aux_y][aux_k];
         else{
-          //for (int i = 0; i < 4 - k / 2; i++) //cout << '\t';
-          //cout << "Chamando min_path(" << aux_x << ", " << aux_y << ", " << aux_k << "){" << endl;
-        
           next_cell_cost = min_path(aux_x, aux_y, aux_k);
           mem_costs[aux_x][aux_y][aux_k] = next_cell_cost;
-          //cout << "Novo valor(em cima) : mem_costs[" << aux_x << "][" << aux_y << "][" << aux_k << "] = " << next_cell_cost << endl; 
-          //next_cell_cost += move_cost;
-
-          //cout << "} (" << aux_k << ")" << endl;
         }
 
         new_cost = next_cell_cost + move_cost;
-        //cout << "Newcost  = " << next_cell_cost << " + " << move_cost << " = " << new_cost << endl;
-        //for (int i = 0; i < 4 - k / 2; i++) //cout << '\t';
-        ////cout << "Para a pos = (" << aux_x << ", " << aux_y << ") =>  cost antigo: " << move_cost << "; newcost: " << new_cost << endl;
 
-        if(new_cost < min_cost){ // atualiza custo se for o menor ate agora
-          //cout << "Newcost: " << new_cost << " menor do que mincost: " << min_cost << endl;
-          
-          min_x = aux_x;
-          min_y = aux_y;
-          min_cost = new_cost;
-        }
+        if(new_cost < min_cost) min_cost = new_cost;
 
       }
-      //costs.push_back(new_cost);
-      //cout << dir << endl;
     }
-    //int min = *(min_element(costs.begin(), costs.end()));
-    //sort(costs.begin(), costs.end());
-    //min_cost = costs[0];
 
-    /*if(aux_k > 0){
-      mem_costs[min_x][min_y][aux_k] = min_cost;
-      cout << "Novo valor(embaixo) : mem_costs[" << min_x << "][" << min_y << "][" << aux_k << "] = " << min_cost << endl;    
-    }*/
-    //cout << "Min cost: " << min << endl;//", max: " << costs[3] << endl;
-
-    return min_cost;// + cost;
+    return min_cost;
   }
 };
 int main()
 {
     int n, m, k;
-    int i,j;
     cin >> n >> m >> k;
 
     solver *solver_obj = new solver(n, m);    
@@ -217,7 +173,6 @@ int main()
     {
         for (int j = 0; j < m; j++)
         {
-            //cout << "Custo maximo (" << i << ", " <<  j << "): " 
             cout << solver_obj->min_path(i, j, k) << " ";
         }
         cout << endl;
