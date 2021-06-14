4e3c36e2244b2b2d9c61fca08f6bcf2cd626a559
APF2000 <arthurpfonseca@gmail.com>
2021-06-13 23:11:39 -0300

    Calculo na vertical tambem

--- Listas/Arrays-matriz-funcoes - Obi 2020/Rectangles/rectangles.cpp
+++ Listas/Arrays-matriz-funcoes - Obi 2020/Rectangles/rectangles.cpp
@@ -7,25 +7,8 @@ int binom(int n)
     return (1 << n) - 1;
 }
 
-int main()
+int sum_sets(vector<vector<int>> mt, int n, int m)
 {
-    int n, m;
-    cin >> n >> m;
-
-    vector<vector<int>> mt;
-
-    for (int i = 0; i < n; i++)
-    {
-        vector<int> vaux;
-        for (int j = 0; j < m; j++)
-        {
-            int aux;
-            cin >> aux;
-            vaux.push_back(aux);
-        }
-        mt.push_back(vaux);
-    }
-
     int sum = 0;
 
     for (vector<int> v : mt)
@@ -56,25 +39,56 @@ int main()
                 }
                 pow = 1;
             }
-            /*if(j == 0 && v[0] == v[m - 1]) zpass = false;
-            else
-            {
-                pow++;
-                if(j == m - 1){
-                    if(!zpass) pow++;
-                    sum += binom(pow);
-                }else
-                {
-
-                }
-            }*/
         }
 
         for(int s : sums) sum += s;
         cout << sum << endl;
     }
+}
 
+void transpose(vector<vector<int>> *mt, int n, int m)
+{
+    vector<vector<int>> maux;
+    vector<int> vaux(n);
+    for (int j = 0; j < m; j++) maux.push_back(vaux);
 
+    for (int i = 0; i < n; i++)
+    {
+        vector<int> vaux;
+        for (int j = 0; j < m; j++)
+        {
+            maux[j][i] = (*mt)[i][j];
+        }
+    }
+
+    (*mt) = maux;
+}
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> mt;
+
+    for (int i = 0; i < n; i++)
+    {
+        vector<int> vaux;
+
+        for (int j = 0; j < m; j++)
+        {
+            int aux;
+            cin >> aux;
+            vaux.push_back(aux);
+        }
+        mt.push_back(vaux);
+    }
+
+    int sum = 0;
+
+    sum += sum_sets(mt, n, m);
+    transpose(&mt, n, m);
+    sum += sum_sets(mt, m, n);
 
     return 0;
 }
