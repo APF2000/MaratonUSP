e720cac4527c9512bfc49bb346901ff5d983ca61
APF2000 <arthurpfonseca@gmail.com>
2021-06-24 15:30:09 -0300

    Rascunho ascend

--- Listas/Ordenacao-Complexidade-BB-TwoPointers/Ascend_Sequence/ascend_sequence.cpp
+++ Listas/Ordenacao-Complexidade-BB-TwoPointers/Ascend_Sequence/ascend_sequence.cpp
@@ -9,37 +9,22 @@ int main()
 {
     int n;
     cin >> n;
-    vector<int> cards;
-    int dir,esq;
-    int sereja = 0, dima = 0;
-    int aux;
-    bool vez_sereja = true;
-    for(int esq = 0; esq < n; esq++ ){
-      cin >> aux;
-      cards.push_back(aux);
-    }
-    while(cards.size() > 0){
-      esq = cards[0];
-      dir = cards[cards.size() - 1];
-      if(esq > dir){
-        aux = esq;
-        cards.erase(cards.begin());
-      } else {
-        aux = dir;
-        cards.pop_back();
+
+    vector<vector<int>> seqs;
+
+    for(int i = 0; i < n; i++ ){
+      int l;
+      cin >> l;
+      vector<int> aux(l);
+      for (int j = 0; j < l; j++)
+      {
+        cin >> aux[j];
       }
-      if(vez_sereja){
-        sereja+=aux;
-        vez_sereja = false;
-      } else{
-        dima += aux;
-        vez_sereja = true;
-      }
+      seqs.push_back(aux);
     }
-    cout << sereja << " " << dima << endl;
+
+    vector<vector<int>> asc, n_asc;
+    
+    
     return 0;
 }
-/*
-7
-1 2 3 4 5 6 7
-*/
