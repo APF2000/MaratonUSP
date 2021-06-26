5db0796cc7180aa30704b6da2a180920814842f9
APF2000 <arthurpfonseca@gmail.com>
2021-06-26 12:56:58 -0300

    Resetando ideias

--- Listas/Ordenacao-Complexidade-BB-TwoPointers/Ascend_Sequence/ascend_sequence.cpp
+++ Listas/Ordenacao-Complexidade-BB-TwoPointers/Ascend_Sequence/ascend_sequence.cpp
@@ -13,13 +13,6 @@ struct ascend_sequence
 
 typedef struct ascend_sequence asc_seq;
 
-int binomial(int i){
-// n!/2!(n-2)! - n_asc!/2 
-    int binom;
-    binom = i*(i-1)/2;
-    return binom;
-}
-
 int main()
 {
     int n;
@@ -37,53 +30,9 @@ int main()
       }
       seqs.push_back(aux);
     }
-    // 1 2 3 4
-    // 
-    vector<vector<int>> asc;
-    vector<asc_seq> n_asc;
     
-   for(vector<int> vec: seqs){
-      int aux_menor = vec[0];
-      int aux_maior = vec[0];
-      bool isAsc = false;
-      for (unsigned int i = 1; i < vec.size(); i++)
-      {
-          if(vec.at(i) < aux_menor){
-            aux_menor = vec.at(i);
-          }
-          if(vec.at(i) > aux_maior){
-            aux_maior = vec.at(i);
-          }
-          if(vec.at(i) > aux_menor){
-            isAsc = true;
-            break;
-          }
-      }
-      if(isAsc){
-        asc.push_back(vec);
-      }
-      else{
-        asc_seq temp;
-        temp.min = aux_menor;
-        temp.max = aux_maior;
-        temp.v = vec;
-        n_asc.push_back(temp);
-      }
-    }
     unsigned long count = 0;
-    count += asc.size()*asc.size() + 2*asc.size()*n_asc.size();
     
-    for (int i = 0; i < n_asc.size(); i++)
-    {
-      for (int j = 0; j < n_asc.size(); j++)
-      {
-        asc_seq a = n_asc[i], b = n_asc[j];
-        if(a.max > b.min){
-          count ++;
-        }
-      }
-      
-    }
     cout << count << endl;
     return 0;
 }
