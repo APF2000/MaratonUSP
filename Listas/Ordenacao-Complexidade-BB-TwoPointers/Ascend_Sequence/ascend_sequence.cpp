c5156d56bf6922a16d1bb3f2f8923df54e5cfe16
APF2000 <arthurpfonseca@gmail.com>
2021-06-26 14:04:01 -0300

    Criando arvore e printando ela

--- Listas/Ordenacao-Complexidade-BB-TwoPointers/Ascend_Sequence/ascend_sequence.cpp
+++ Listas/Ordenacao-Complexidade-BB-TwoPointers/Ascend_Sequence/ascend_sequence.cpp
@@ -1,38 +1,109 @@
 //https://codeforces.com/contest/1284/problem/B
+
 #include <vector>
 #include <iostream>
+#include <map>
+#include <algorithm> // min_element, max_element
 
 using namespace std;
 
-struct ascend_sequence
+struct ascend_node
 {
   int max;
   int min;
-  vector<int> v;
+  map<bool, struct ascend_node*> sons; // direita e esquerda
+  //vector<int> v;
 };
 
-typedef struct ascend_sequence asc_seq;
+typedef struct ascend_node asc_node;
+
+void insert_node(asc_node *root, asc_node *new_node, long unsigned *count)
+{
+	asc_node *last = root, *next = root;
+
+	while(next != NULL)
+	{
+		bool is_greater = (new_node->max > next->min);
+
+		next = last->sons[is_greater];
+		if(next == NULL) last->sons[is_greater] = new_node;
+		last = next;
+
+		(*count)++;
+	}
+}
+
+void print_spaces(int qtty)
+{ 
+	for (int i = 0; i < qtty; i++) cout << "\t";
+ }
+
+void print_tree(asc_node *root, int depth)
+{
+	if(root == NULL) return;
+
+	cout << "(" << root->min << ", " << root->max << ")-> " << endl;
+
+	print_spaces(depth + 1);
+	cout << "e: {";
+	print_tree(root->sons[true], depth + 1);
+	print_spaces(depth + 1);
+	cout << "} " << endl;
+
+	print_spaces(depth + 1);
+	cout << "d: {";
+	print_tree(root->sons[false], depth + 1);
+	print_spaces(depth + 1);
+	cout << "} " << endl;
+}
 
 int main()
 {
     int n;
     cin >> n;
 
-    vector<vector<int>> seqs;
+    vector<asc_node> seqs;
 
     for(int i = 0; i < n; i++ ){
-      int l;
-      cin >> l;
-      vector<int> aux(l);
-      for (int j = 0; j < l; j++)
-      {
-        cin >> aux[j];
-      }
-      seqs.push_back(aux);
+		int l;
+		cin >> l;
+
+		vector<int> aux(l);
+		for (int j = 0; j < l; j++)
+		{
+			cin >> aux[j];
+		}
+
+		int min = *(min_element(aux.begin(), aux.end()));
+		int max = *(max_element(aux.begin(), aux.end()));
+
+		asc_node anode;
+		anode.min = min;
+		anode.max = max;
+
+		seqs.push_back(anode);
     }
-    
+
     unsigned long count = 0;
-    
-    cout << count << endl;
+
+	// Ida
+    asc_node *root = &seqs[0];
+    for (int i = 1; i < n; i++)
+    {
+		asc_node *new_node = &seqs[i];
+		insert_node(root, new_node, &count);
+    }
+	print_tree(root, 0);
+
+	// Volta
+	root = &seqs[n - 1];
+    for (int i = n - 2; i >= 0; i--)
+    {
+		asc_node *new_node = &seqs[i];
+		insert_node(root, new_node, &count);
+    }
+
+	cout << count << endl;
+
     return 0;
 }
