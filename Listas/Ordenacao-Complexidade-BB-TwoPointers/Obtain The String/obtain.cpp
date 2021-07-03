7566f4624df6c7edfc867335ea6f969bd7f0b128
Sergio <sergio.mcontente@gmail.com>
2021-07-02 21:02:21 -0300

    sergio-novos

--- Listas/Ordenacao-Complexidade-BB-TwoPointers/Obtain The String/obtain.cpp
+++ Listas/Ordenacao-Complexidade-BB-TwoPointers/Obtain The String/obtain.cpp
@@ -1,3 +1,4 @@
+//https://codeforces.com/contest/1295/problem/C
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -6,21 +7,34 @@ int main()
 {
     string s, t, z;
     string prov;
-    int T;
+    int T, com ;
+	bool achou = false;
     cin >> T;
     for (int i = 0; i < T; i++)
     {
         cin >> s;
         cin >> t;
+		z = "";
+		com = 0;
         for(int j = 0; j < t.length(); j++){
             //cout << "T[" << j <<"]" << t[j] << endl;
-            for(int k = 0; k < s.length(); k++){
+			achou = false;
+            for(int k = com; k < s.length(); k++){
                 if(s[k] == t[j]){
-                    prov = s[k];
-                   z.append(prov);
-                   cout << "z[" << j <<"]" << z << endl;
+					for(int a = 0; a < z.length(); a++){
+						if(s[k] == z[a]){
+							achou = true;
+						}
+					}
+					if(!achou){
+						prov = s[k];
+                   		z.append(prov);
+						achou = false;
+                   		cout << "z[" << j <<"]" << z << endl;
+					}
                 }
             }
+			com++;
         }
     }
     return 0;
