c33c453491d45673df48f8025ea12e1f12a51c92
Sergio <sergio.mcontente@gmail.com>
2021-07-04 11:43:22 -0300

    obtain

--- Listas/Ordenacao-Complexidade-BB-TwoPointers/Obtain The String/obtain.cpp
+++ Listas/Ordenacao-Complexidade-BB-TwoPointers/Obtain The String/obtain.cpp
@@ -3,39 +3,78 @@
 
 using namespace std;
 
+int bb(vector<int> v, int index, int esq, int dir){
+	int m;
+	m = (esq + dir)/2;
+	if(m == esq){
+		/*if(m == v.size()-1){
+			return -1;
+		} */
+		return m+1;
+	}
+	if(v[m] < index){
+		return bb(v, index, m, dir);
+	}
+	else if(v[m] > index){
+		return bb(v, index, esq, m);
+	}
+	else{
+		/*if(m == v.size()-1){
+			return -1;
+		}*/
+		return m+1;
+	}
+}
+// 1 3 5 8 10 15 23
+// 1 4 7 9 13 14
 int main()
 {
     string s, t, z;
     string prov;
-    int T, com ;
-	bool achou = false;
+    int T, op;
+	bool no_letter = false;
     cin >> T;
     for (int i = 0; i < T; i++)
     {
         cin >> s;
         cin >> t;
 		z = "";
-		com = 0;
-        for(int j = 0; j < t.length(); j++){
-            //cout << "T[" << j <<"]" << t[j] << endl;
-			achou = false;
-            for(int k = com; k < s.length(); k++){
-                if(s[k] == t[j]){
-					for(int a = 0; a < z.length(); a++){
-						if(s[k] == z[a]){
-							achou = true;
-						}
-					}
-					if(!achou){
-						prov = s[k];
-                   		z.append(prov);
-						achou = false;
-                   		cout << "z[" << j <<"]" << z << endl;
-					}
-                }
-            }
-			com++;
+		op = 0;
+		map<char, vector<int>> pos;
+        for(int j = 0; j < s.length(); j++){
+			char key = s.at(j);
+            if(pos.find(j) == pos.end()){
+				pos[key] = {j};
+			}
+			else{
+				pos[key].push_back(key);
+			}
         }
+		int last_index = 0, new_index;
+		for(int k = 0; k < t.size(); k++){
+			char key = t.at(k);
+			if(pos.find(key) != pos.end()){
+				new_index = bb(pos[key], last_index, 0, pos[key].size()-1);
+				if(new_index == pos[key].size() - 1){
+					new_index = last_index;
+				}
+				//cout << new_index << endl; 
+				if(new_index <= last_index){
+					op++;
+				}
+				last_index = new_index;
+			}
+			else{
+				no_letter = true;
+				break;
+			}
+		}
+		if(no_letter){
+			cout << "-1" << endl;
+		}
+		else{
+			cout << op << endl;
+		}
     }
     return 0;
 }
