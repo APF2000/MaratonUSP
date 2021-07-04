b4272ed9e5acf125ed89b145cbc745e9e3362219
APF2000 <arthurpfonseca@gmail.com>
2021-07-04 14:01:46 -0300

    Agora passa no exemplo inventado

--- Listas/Ordenacao-Complexidade-BB-TwoPointers/Obtain The String/obtain.cpp
+++ Listas/Ordenacao-Complexidade-BB-TwoPointers/Obtain The String/obtain.cpp
@@ -5,20 +5,33 @@ using namespace std;
 
 int bb(vector<int> v, int index, int esq, int dir){
 	int m;
+	int size = v.size();
 	m = (esq + dir)/2;
-	if(m == esq || v[m] == index){
-		if(m == v.size()-1){
+	// cout << "dir: " << dir << "; esq: " << esq << "; index: " << index << endl;
+	// cout << "size: "<< size << endl;
+
+	if(esq >= dir)
+	{
+		if(index >= v[m]) return v[(m + 1) % size];
+		else return v[m];
+		//if(v[m] < index) return v[m];
+		//else if(v[m] > index) return v[(m - 1 + size) % size];
+	}
+	/*if(m == esq || v[m] == index){
+		return v[(m + 1) % v.size()];
+		/*if(m == v.size()-1){
 			return v[0];
 		}
-		return v[m];
-	}
+		return v[m];*
+	}*/
 	if(v[m] < index){
-		return bb(v, index, m, dir);
+		return bb(v, index, m + 1, dir);
 	}
 	else if(v[m] > index){
-		return bb(v, index, esq, m);
+		return bb(v, index, esq, m - 1);
 	}
 	else{
+		return v[(m + 1) % v.size()];
 		/*if(m == v.size()-1){
 			return -1;
 		}*/
@@ -54,9 +67,12 @@ int main()
 		for(int k = 0; k < t.size(); k++){
 			char key = t.at(k);
 			if(pos.find(key) != pos.end()){
+				// cout << "key: " << key << endl; 
+				// cout << "lastindex: " << last_index << endl;
+				
 				new_index = bb(pos[key], last_index, 0, pos[key].size()-1);
 
-				//cout << "newindex: " << new_index << endl; 
+				//cout << "newindex: " << new_index << endl;
 
 				// if(new_index == pos[key].size()){
 				// 	new_index = last_index;
@@ -65,6 +81,7 @@ int main()
 				if(new_index <= last_index){
 					ops++;
 				}
+				//cout << "newops: " << ops << endl;
 				last_index = new_index;
 			}
 			else{
