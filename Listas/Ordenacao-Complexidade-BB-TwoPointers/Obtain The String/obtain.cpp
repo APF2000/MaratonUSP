ef16ae37699b361a82e1699841968c235c548cfe
APF2000 <arthurpfonseca@gmail.com>
2021-07-04 14:43:19 -0300

    Erro de novo no exemplo 2: ta dando 26 -1 4 10

--- Listas/Ordenacao-Complexidade-BB-TwoPointers/Obtain The String/obtain.cpp
+++ Listas/Ordenacao-Complexidade-BB-TwoPointers/Obtain The String/obtain.cpp
@@ -24,6 +24,21 @@ int bb(vector<int> v, int index, int esq, int dir){
 	}
 }
 
+struct v_and_last
+{
+	int last;
+	vector<int> v;
+};
+
+typedef struct v_and_last vl;
+
+int next_index(vl *el)
+{
+	int *last = &(el->last);
+	*last = (*last + 1) % el->v.size();
+
+	return( el->v[*last] );
+}
 
 int main()
 {
@@ -35,18 +50,22 @@ int main()
     {
 		bool no_letter = false;
 		int ops = 1;
-		map<char, vector<int>> pos;
+		map<char, vl> pos;
 
         cin >> s;
         cin >> t;
 
         for(int j = 0; j < s.length(); j++){
 			char key = s.at(j);
+			vl aux;
+
             if(pos.find(key) == pos.end()){
-				pos[key] = {j};
+				aux.last = 0;
+				aux.v = {j};
+				pos[key] = aux;
 			}
 			else{
-				pos[key].push_back(j);
+				pos[key].v.push_back(j);
 			}
         }
 
@@ -55,7 +74,8 @@ int main()
 			char key = t.at(k);
 			if(pos.find(key) != pos.end()){
 
-				new_index = bb(pos[key], last_index, 0, pos[key].size()-1);
+				//new_index = bb(pos[key], last_index, 0, pos[key].size()-1);
+				new_index = next_index(&pos[key]);
 
 				if(new_index <= last_index){
 					ops++;
