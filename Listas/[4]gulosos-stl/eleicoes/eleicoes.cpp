aa53a4849633cfd195647601825d3d151341808b
APF2000 <arthurpfonseca@gmail.com>
2021-07-09 13:15:10 -0300

    Gulosos: eleicoes

---
+++ Listas/[4]gulosos-stl/eleicoes/eleicoes.cpp
@@ -0,0 +1,22 @@
+// https://br.spoj.com/problems/ELEICOES/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n; 			// 1 <= n <= 100.000 = 10^5
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		int xi; 	// 1 < Xi ≤ 1.000.000.000 = 10^9
+		cin >> xi;
+
+		
+	}
+	
+
+	return 0;
+}
