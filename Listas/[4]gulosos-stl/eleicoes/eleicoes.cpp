7634a12f7821c9636006932755493392c7e5a470
APF2000 <arthurpfonseca@gmail.com>
2021-07-09 13:52:29 -0300

    Passa nos exemplos dados

--- Listas/[4]gulosos-stl/eleicoes/eleicoes.cpp
+++ Listas/[4]gulosos-stl/eleicoes/eleicoes.cpp
@@ -1,22 +1,40 @@
 // https://br.spoj.com/problems/ELEICOES/
 
 #include<iostream>
+#include<map>
 
 using namespace std;
 
 int main()
 {
-	int n; 			// 1 <= n <= 100.000 = 10^5
+	long n; 			// 1 <= n <= 100.000 = 10^5
 	cin >> n;
 
-	for (int i = 0; i < n; i++)
+	long max = -1;
+	long winner = -1;
+	map<long, long> cand_to_votes;
+	for (long i = 0; i < n; i++)
 	{
-		int xi; 	// 1 < Xi ≤ 1.000.000.000 = 10^9
+		long xi; 	// 1 < Xi ≤ 1.000.000.000 = 10^9
 		cin >> xi;
 
-		
+		if(cand_to_votes.find(xi) == cand_to_votes.end()) // not counted yet
+		{
+			cand_to_votes[xi] = 1;
+		}else
+		{
+			cand_to_votes[xi]++;
+		}
+
+		long vote_count = cand_to_votes[xi];
+		if(vote_count > max)
+		{
+			max = vote_count;
+			winner = xi;
+		}
 	}
 	
+	cout << winner << endl;
 
 	return 0;
 }
