cd6888c7103859e20dbd5edfdc82786ffc2b3827
APF2000 <arthurpfonseca@gmail.com>
2021-07-11 10:12:51 -0300

    Inputs do problema

--- Listas/[4]gulosos-stl/third-party-software/third-party-software.cpp
+++ Listas/[4]gulosos-stl/third-party-software/third-party-software.cpp
@@ -6,6 +6,18 @@ using namespace std;
 
 int main()
 {
+	int n; // 1 ≤ n≤ 200.000
+	int m; // 1 ≤ m≤ 10^9 
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+	{
+		int min, max;
+		cin >> min >> max;
+
+		
+	}
 	
 
 	return 0;
