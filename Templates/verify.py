57444a82e4fa65c8a86babe5d41490ccc8b927b4
APF2000 <arthurpfonseca@gmail.com>
2021-07-17 13:38:36 -0300

    Agora verifica antes se tem output.txt

--- Templates/verify.py
+++ Templates/verify.py
@@ -13,8 +13,11 @@ tmp_name = 'tmp.txt'
 os.system('g++ ' + cpp_name)
 for in_file_name in in_files:
 
-	out_file_name = re.sub(template_in_name, template_out_name, in_file_name)
-	expected = open(out_file_name, 'r').read() 
+	try:
+		out_file_name = re.sub(template_in_name, template_out_name, in_file_name)
+		expected = open(out_file_name, 'r').read() 
+	except FileNotFoundError:
+		continue
 
 	os.system(f'cat %s | ./a.out > %s' % (in_file_name, tmp_name))
 	received = open(tmp_name, 'r').read() 
