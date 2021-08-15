c71f614b8c2399654b21aefcf560e695104ed80a
APF2000 <arthurpfonseca@gmail.com>
2021-08-15 12:00:02 -0300

    Melhorando testador oficial da seleçao brasileira

--- Templates/verify.py
+++ Templates/verify.py
@@ -3,6 +3,8 @@ import os
 import sys
 import re
 
+info_pattern = r'\[INFO\][^\r\n]*(\n|\r\n)'
+
 template_in_name = 'input.txt'
 template_out_name = 'output.txt'
 in_files = glob.glob('*' + template_in_name)
@@ -20,13 +22,14 @@ for in_file_name in in_files:
 		continue
 
 	os.system(f'cat %s | ./a.out > %s' % (in_file_name, tmp_name))
-	received = open(tmp_name, 'r').read() 
+	received_log = open(tmp_name, 'r').read() 
+	received = re.sub(info_pattern, '', received_log)
 	
 	if(expected.strip() == received.strip()):
 		print(f'Test %s ok' % in_file_name)
 	else:
 		print(f'Test %s not ok' % in_file_name)	
-		print(f'received:\n%s' % received)	
+		print(f'received:\n%s' % received_log)	
 		print(f'expected:\n%s' % expected)		
 
 	os.system('rm ' + tmp_name)
