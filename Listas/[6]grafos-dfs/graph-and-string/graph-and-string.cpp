84c64aa76a62f7b1a3d10e78135294f37187f67a
APF2000 <arthurpfonseca@gmail.com>
2021-09-03 19:27:18 -0300

    Nao passa no teste 3 do judge

--- Listas/[6]grafos-dfs/graph-and-string/graph-and-string.cpp
+++ Listas/[6]grafos-dfs/graph-and-string/graph-and-string.cpp
@@ -25,14 +25,47 @@ class node{
 string s;
 vector<node> nodes;
 
-bool tem_resp(int n, int id, bool last_c){
+bool tem_resp(int n, int id, bool switch_a_c){
+	// se ficar encurralado, verifica os nos adjacentes e ve se e possivel
+	//cout << "id: " << id << endl;
 	
+	unordered_set<int> not_adj = nodes[id].not_adj;
+	char aux_letter = 0;
+
+	if(not_adj.size() == 0) 	aux_letter = 'b';
+	else if(switch_a_c) 		aux_letter = 'a';
+	else						aux_letter = 'c';
+
+	nodes[id].letter = aux_letter;
+	s[id] = aux_letter;
+	nodes[id].check = true;
+
+	bool encurralado = true;
+	for(int id_not_adj : not_adj) // olha os nao adjacentes
+	{
+		if(!nodes[id_not_adj].check) encurralado = false;
+	}
+
+	if(encurralado && aux_letter != 'b')
+	{
+		for(int id_not_adj : not_adj) // olha os nao adjacentes
+		{
+			if(nodes[id_not_adj].letter == aux_letter) // se o meu e 'a', o outro tem que ser 'c' e vice-versa
+			{
+				return false;
+			}
+		}
+	}
+	if(aux_letter != 'b') switch_a_c = !switch_a_c;
+
+	if(id == n - 1) 	return true;
+	return tem_resp(n, id + 1, switch_a_c);
 }
 
 int main()
 {
-	int n,m;
-	vector<node> nodes;
+	int n, m;
+	//vector<node> nodes;
 	unordered_set<int> aux_s;
 
 	cin >> n >> m;
@@ -56,6 +89,7 @@ int main()
 		cin >> n1 >> n2;
 		n1--;
 		n2--;
+		//cout << "n1: " << n1 << ", n2: " << n2 << endl;
 		nodes[n1].not_adj.erase(n2); // tira os que tem conexao
 		nodes[n2].not_adj.erase(n1);
 	}
