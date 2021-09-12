ecd5d0f1709d71e5d96ecd1fc497277a511e3e45
APF2000 <arthurpfonseca@gmail.com>
2021-09-12 10:55:46 -0300

    Nova logica, mas ferrou o exemplo 26

--- Listas/[6]grafos-dfs/graph-and-string/graph-and-string.cpp
+++ Listas/[6]grafos-dfs/graph-and-string/graph-and-string.cpp
@@ -26,86 +26,48 @@ class node{
 string s;
 vector<node> nodes;
 
-bool tem_resp(int n, int id, bool switch_a_c){
-	// se ficar encurralado, verifica os nos adjacentes e ve se e possivel
-		
-	//cout << "id: " << id << endl;
+char add_letter(bool is_a, int id)
+{
+	char aux_letter = (is_a ? 'a' : 'c');
+
+	s[id] = aux_letter;
+	nodes[id].letter = aux_letter;
+
+	return aux_letter;
+}
+
+bool tem_resp(int n, int id, bool is_a)
+{
+	if(id >= n) return true; // passou do ultimo
+
+	nodes[id].check = true; // elemento foi checado
+
+	char previous_letter = s[id];
+	char aux_letter = add_letter(is_a, id);
+
+	// entrou em contradicao
+	if(previous_letter != BLANK && previous_letter != aux_letter) return false;
+
 	unordered_set<int> not_adj = nodes[id].not_adj;
-	unordered_set<int> adj = nodes[id].adj;
-
-	//cout << "size nadj: " << nodes[id].not_adj.size() << endl;
-	//cout << "size adj: " << nodes[id].adj.size() << endl;
-	char aux_letter = 0;
-
-	if(!nodes[id].check)
+	if(not_adj.size() == 0) // primeira(s) letra(s) e(sao) b
 	{
-
-		if(nodes[id].not_adj.size() == 0) 	aux_letter = 'b';
-		else if(switch_a_c) 		aux_letter = 'a';
-		else						aux_letter = 'c';
-		
-		nodes[id].letter = aux_letter;
-		s[id] = aux_letter;
-		nodes[id].check = true;
-
-		if(aux_letter == 'b')
-		{
-			for(node nd : nodes)
-			{
-				nd.adj.erase(id);
-			}
-		}
-
-	}else
-	{
-		aux_letter = nodes[id].letter;
-		switch_a_c = (aux_letter == 'a'); // cuidado quando for B
+		s[id] = 'b';
+		return tem_resp(n, id + 1, is_a);
 	}
 
-//cout << "antes encurralado" << endl;
-	bool encurralado = true;
-	unordered_set<int> aux_not_adj = nodes[id].not_adj;
-	for(int id_not_adj : aux_not_adj) // olha os nao adjacentes
+	for(int id_not_adj : not_adj)
 	{
-//cout << "antes if 1 idnotadj: " << id_not_adj << endl;
-//cout << "size notasj " <<  nodes[id].not_adj.size() << endl;
-		if(nodes[id_not_adj].letter == aux_letter) 
+		node *aux_node = &nodes[id_not_adj];
+		if(aux_node->check)
 		{
-			return false; 		
-		}
-		
-//cout << "antes if 2" << endl;
-		if(nodes[id_not_adj].check) // tira da lista um do outro
-		{ 
-			nodes[id_not_adj].not_adj.erase(id);
-			nodes[id].not_adj.erase(id_not_adj);
-		}
-		else{
-			encurralado = false;
-			bool possivel = tem_resp(n, id_not_adj, !switch_a_c);
-			if(!possivel) return false;
+			if(aux_node->letter == aux_letter) return false;
+		}else
+		{
+			if(!tem_resp(n, id_not_adj, !is_a)) return false;
 		}
 	}
 
-	if(encurralado && aux_letter != 'b')
-	{
-	//cout << "batata" << endl;
-		for(int id_adj : nodes[id].adj) // olha os adjacentes
-		{
-			char letter = nodes[id_adj].letter;
-			if(letter != aux_letter && letter != 'b' && letter != BLANK)
-			{
-				return false;
-			}
-		}
-	}
-
-	//cout << "size nadj: " << nodes[id].not_adj.size() << endl;
-	//cout << "size adj: " << nodes[id].adj.size() << endl;
-		
-	if(id == n - 1) 	return true;
-	
-	return tem_resp(n, id + 1, switch_a_c);
+	return true;
 }
 
 int main()
@@ -126,7 +88,6 @@ int main()
 		node aux(aux_s);
 		aux.not_adj.erase(i);
 		
-
 		nodes.push_back(aux);
 		s.push_back(BLANK);
 	}
@@ -146,6 +107,11 @@ int main()
 	}
 
 	if(tem_resp(n, 0, true)){
+		for(unsigned long i = 0; i < s.size(); i++)
+		{
+			if(s[i] == BLANK) s[i] = 'b'; // quem nao tinha sido setado ainda
+		}
+
 		cout << "Yes" << endl;
 		cout << s << endl;
 	}else{
