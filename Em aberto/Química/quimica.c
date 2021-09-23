feeeb73059e69d707b1c407439f51a8c2d45c304
sergiofuentes <sergioagf10@gmail.com>
2021-09-23 20:15:42 -0300

    Passando os Em Abertos para os Resolvidos(agora sim foi tudo)

--- Em aberto/Química/quimica.c
+++ Em aberto/Química/quimica.c
@@ -1,3 +1,4 @@
+// https://www.urionlinejudge.com.br/judge/pt/problems/view/1326
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- Resolvidos/DDD/ddd.cpp
+++ Resolvidos/DDD/ddd.cpp
@@ -1,3 +1,4 @@
+//https://www.urionlinejudge.com.br/judge/pt/problems/view/1050
 #include <iostream>
 #include <string>
 
