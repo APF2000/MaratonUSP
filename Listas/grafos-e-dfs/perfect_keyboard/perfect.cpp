05d895ee1b229d03384d0ba6a395e31e73ab8af9
sergio <sergio.mcontente@gmail.com>
2021-09-26 12:02:41 -0300

    keyboard errado

--- Listas/grafos-e-dfs/perfect_keyboard/perfect.cpp
+++ Listas/grafos-e-dfs/perfect_keyboard/perfect.cpp
@@ -1,10 +1,12 @@
+//https://codeforces.com/problemset/problem/1303/C
+
 #include<iostream>
 #include <string>
 #include <vector>
 #include<map>
 using namespace std;
 
-bool analaisador(string s, vector<char> *kbrd){
+bool analisador(string s, vector<char> *kbrd){
 	char analise;
 	int position = 0;
 	int visitados [26];
@@ -15,13 +17,13 @@ bool analaisador(string s, vector<char> *kbrd){
 	for (int i = 1; i < s.size(); i++)
 	{
 		if(visitados[s[i] - 'a'] == 0){
-			if(position == kbrd->size() - 1){
+			if(position == kbrd->size() - 1){ //insere no fim
 				kbrd->push_back(s[i]);
 				visitados[s[i] - 'a'] = 1;
 				position++;
 			}
 			else{
-				if(position == 0){
+				if(position == 0){ //insere no começo
 					visitados[s[i] - 'a'] = 1;
 					kbrd->insert(kbrd->begin(), s[i]);
 				}
@@ -30,7 +32,7 @@ bool analaisador(string s, vector<char> *kbrd){
 				}
 			}
 		}
-		else{
+		else{ 
 			if(position == kbrd->size() - 1){
 				if(kbrd->at(kbrd->size()-2) == s[i]){
 					position --;
@@ -77,7 +79,7 @@ int main(){
 	for (int i = 0; i < T; i++)
 	{
 		cin >> s;
-		isPossible = analaisador(s, &kbrd);
+		isPossible = analisador(s, &kbrd);
 		if(isPossible){
 			cout << "YES" << endl;
 			for(int i = 0; i < kbrd.size() - 1; i++){
