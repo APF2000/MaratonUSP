e00408fb347510013ac92638b491d238fcb45522
APF2000 <arthurpfonseca@gmail.com>
2021-10-10 14:09:50 -0300

    :bug: Time limit exceeded on test 28

--- Listas/[6]grafos-dfs/two-fairs/two_fairs.cpp
+++ Listas/[6]grafos-dfs/two-fairs/two_fairs.cpp
@@ -35,37 +35,37 @@ unordered_set<long> find_direct_paths_adj_to_b(map< long, node > graph, long a, 
 
 	while(!nodes_to_visit.empty())
 	{
-		cout << "================" << endl;
+		//cout << "================" << endl;
 
 		long key_node = nodes_to_visit.front();
-		cout << "keynode " << key_node << endl;
+		//cout << "keynode " << key_node << endl;
 		//visited_nodes.insert(key_node);
 
 		// not visited into queue
 		for(long adj : graph[key_node].adjs)
 		{
 			if(adj == b){
-				cout << "touched fair from " << key_node << " to " << adj << endl;
+				//cout << "touched fair from " << key_node << " to " << adj << endl;
 				if(key_node != a && key_node != b)
 				{
-					cout << "insert " << key_node << " to directpath" << endl;
+					//cout << "insert " << key_node << " to directpath" << endl;
 					direct_paths.insert(key_node);
 				}
 			} 
 			else if(visited_nodes.find(adj) == visited_nodes.end())
 			{ 
-				cout << "notvisited " << adj << endl;
+				//cout << "notvisited " << adj << endl;
 				nodes_to_visit.push(adj);
 				visited_nodes.insert(adj);
 			}
 		}
 
 		nodes_to_visit.pop();
-		for(queue<long> aux = nodes_to_visit; !aux.empty(); aux.pop()) 
-			cout << "nodes_to_visit: " << aux.front() << endl;
+		//for(queue<long> aux = nodes_to_visit; !aux.empty(); aux.pop()) 
+			//cout << "nodes_to_visit: " << aux.front() << endl;
 	}
 
-	for(long el : direct_paths) cout << "direct path: " << el << endl;
+	//for(long el : direct_paths) //cout << "direct path: " << el << endl;
 
 	return direct_paths;
 }
@@ -77,7 +77,7 @@ void remove_dirty_nodes(map< long, node > *graph, long a, long b, unordered_set<
 		long node_key = *(direct_nodes.begin());
 		direct_nodes.erase(node_key);
 
-		cout << "remove node " << node_key << endl;
+		//cout << "remove node " << node_key << endl;
 
 		node n = (*graph)[node_key];
 
@@ -95,24 +95,24 @@ void remove_dirty_nodes(map< long, node > *graph, long a, long b, unordered_set<
 	}
 }
 
-int count_clean_nodes(map< long, node > graph, long a, long b)
+unsigned long long count_clean_nodes(map< long, node > graph, long a, long b)
 {
 	node n = graph[a];
 	unordered_set<long> clean_nodes = {a};
-	int count = 0;
-	cout << "ccccccccccccccccccc" << endl;
+	unsigned long long count = 0;
+	//cout << "ccccccccccccccccccc" << endl;
 
 	while(!clean_nodes.empty())
 	{
 		long node_key = *(clean_nodes.begin());
 		clean_nodes.erase(node_key);
 
-		cout << "erase " << node_key << endl;
+		//cout << "erase " << node_key << endl;
 
 		if(node_key != b && node_key != a)
 		{
-			cout << "count++ " << endl;
-			cout << "nodekey == " << node_key << " != " << b << " != " << a << endl;
+			//cout << "count++ " << endl;
+			//cout << "nodekey == " << node_key << " != " << b << " != " << a << endl;
 			count++;
 		}
 
@@ -134,16 +134,14 @@ int count_clean_nodes(map< long, node > graph, long a, long b)
 	return count;
 }
 
-long solve(map< long, node > graph, long a, long b)
+unsigned long long solve(map< long, node > graph, long a, long b)
 {
 	unordered_set<long> direct_nodes = find_direct_paths_adj_to_b(graph, a, b);
 	remove_dirty_nodes(&graph, a, b, direct_nodes);
-	int clean_a = count_clean_nodes(graph, a, b);
-	int clean_b = count_clean_nodes(graph, b, a);
+	unsigned long long clean_a = count_clean_nodes(graph, a, b);
+	unsigned long long clean_b = count_clean_nodes(graph, b, a);
 
 	return clean_a * clean_b;
-
-	return 0;
 }
 
 void add_edge(map< long, node > *graph, long v1, long v2)
@@ -178,9 +176,9 @@ int main()
 			add_edge(&graph, v2, v1);
 		}
 		
-		long ans = solve(graph, a, b);
+		unsigned long long ans = solve(graph, a, b);
 		cout << ans << endl;
-		cout << "-------------------------------" << endl;
+		//cout << "-------------------------------" << endl;
 	}
 	
 
