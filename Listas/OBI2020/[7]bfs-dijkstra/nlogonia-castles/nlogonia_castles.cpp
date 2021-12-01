8a82f40d9669c9a76095427b46ac3e59e461f8d2
APF2000 <arthurpfonseca@gmail.com>
2021-12-01 20:13:53 -0300

    :bug: Nlogonia castles: faster, but not furious

--- Listas/OBI2020/[7]bfs-dijkstra/nlogonia-castles/nlogonia_castles.cpp
+++ Listas/OBI2020/[7]bfs-dijkstra/nlogonia-castles/nlogonia_castles.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <unordered_set>
-#include <vector>
+#include <stack>
 
 using namespace std;
 
@@ -12,30 +12,78 @@ using namespace std;
 struct node
 {
 	unordered_set<int> adjs;
+	//unordered_map<int, unordered_set<int>> routes;
+	unordered_map<int, int> routes; 
 	int c;
 };
 
 unordered_map<int, node> graph;
 unordered_set<int> visited = {};
+stack<int> path = {};
+
+bool foo(int p, int c)
+{
+	// << "foo\n";
+
+	graph[p].c = c;
+	return true;
+}
 
 bool can_paint(int p, int q, int c)
 {
 	visited.insert(p);
+	//DEBUG(p);
+	//DEBUG(q);
+	//DEBUG(c);
+	// << endl;
 	
-	if(p == q)
+	if(p == q) 
 	{
-		graph[p].c = c;
-		return true;
+			// << "1" << endl;
+		return foo(p, c);
 	}
-
+	
 	for (int child : graph[p].adjs)
 	{
 		if(visited.find(child) != visited.end()) continue;
+		
+		//DEBUG(child);
+
+		unordered_map<int, int> child_routes = graph[p].routes;
+		if( child_routes.find(q) != child_routes.end() )
+		{
+			path.push(child);
+			// << "\n2" << endl;
+			can_paint(child, q, c);
+			return foo(p, c);
+		}
 
 		if(can_paint(child, q, c))
 		{
-			graph[p].c = c;
-			return true;
+			path.push(child);
+
+			stack<int> aux_path = path;
+			while(!aux_path.empty())
+			{
+				int next_in_stack = aux_path.top();
+
+				graph[p].routes[child] = next_in_stack;
+				aux_path.pop();
+			}
+			
+
+			// DEBUG(p);
+			// DEBUG(child);
+			// cout << ": {";
+			// for(int el : graph[p].routes[child])
+			// {
+			// 	cout << el << " ";
+			// }
+			// cout << "}" << endl;
+
+			//cout << "3" << endl;
+			//cin >> child;
+			return foo(p, c);
 		}
 	}
 
@@ -64,7 +112,9 @@ int main()
 		cin >> p >> q >> c;
 
 		can_paint(p, q, c);
+
 		visited = {};
+		path = {};
 	}
 
 	for (int i = 1; i <= n; i++)
