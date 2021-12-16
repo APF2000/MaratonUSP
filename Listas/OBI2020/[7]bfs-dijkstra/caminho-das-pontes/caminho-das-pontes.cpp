052f67cc78f64d96e80fb9e04ebdc24da2868feb
APF2000 <arthurpfonseca@gmail.com>
2021-12-15 21:59:21 -0300

    :memo: [caminho-das-pontes] Djikstra almost ready

--- Listas/OBI2020/[7]bfs-dijkstra/caminho-das-pontes/caminho-das-pontes.cpp
+++ Listas/OBI2020/[7]bfs-dijkstra/caminho-das-pontes/caminho-das-pontes.cpp
@@ -3,9 +3,14 @@
 // #include <bits/stdc++.h>
 
 #include <iostream>
-#include <map>
+#include <cstdio>
 #include <set>
 #include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <queue>
+
+#define MAX_INT ~(1 << 31)
 
 using namespace std;
 
@@ -20,21 +25,87 @@ class conn{
 	
 };
 
-map<int, set<conn>> graph;
+unordered_map<int, set<conn>> graph;
+
+//unordered_map<int, unordered_map<int, bool>> vis_edges;
+unordered_set<int> vis_nodes;
+
+queue<int> next_nodes;
+vector<int> pesos;
+
+int n, m;
+
+int calc_min_holes()
+{
+	next_nodes.push(0);
+	vis_nodes = {0};
+	
+	while( vis_nodes.size() < n )
+	{
+		int this_node = next_nodes.front();
+		next_nodes.pop();
+		
+		vis_nodes.insert(this_node);
+
+		for(conn edge : graph[this_node])
+		{
+			int edge_peso = edge.peso;
+			int adj_id = edge.adj;
+			int best_adj_peso = pesos[adj_id];
+
+			if( vis_nodes.find(adj_id) == vis_nodes.end() ) // never seen
+			{
+				next_nodes.push(adj_id);
+			}
+
+			int best_this_peso = pesos[this_node];
+			if(best_this_peso == MAX_INT)
+			{
+				cout << "EU SOU BURRO" << endl;
+			}
+
+			int new_peso = best_this_peso + edge_peso;
+			if(new_peso < best_adj_peso)
+			{
+				pesos[adj_id] = new_peso;
+			}
+		}
+	}
+
+	return pesos[n - 1];
+}
 
 int main()
 {
-	int n, m;
 	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+	{
+		pesos.push_back( MAX_INT );
+		// for (int j = 0; j < m; j++)
+		// {
+		// 	vis_edges[i][j] = false;
+		// 	vis_edges[j][i] = false;
+		// }
+		
+	}
+	pesos[0] = 0;
+	
+	
 	for (int i = 0; i < m; i++)
 	{
 		int s,t,b;
 		cin >> s >> t >> b;
-		conn n;
-		n.adj = t;
-		n.peso = b;
-		graph[s].insert(n);
+
+		conn co;
+		co.adj = t;
+		co.peso = b;
+		graph[s].insert(co);
 	}
+
+	int min_holes = calc_min_holes();
+	cout << min_holes << endl;
+
 	cout << "Fim" << endl;
 	return 0;
 }
