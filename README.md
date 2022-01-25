39e23c02b36a7da1d6889df309f2b68b15491d71
APF2000 <arthurpfonseca@gmail.com>
2022-01-25 19:49:36 -0300

    Testing 2nd place solution

--- README.md
+++
@@ -1,2 +0,0 @@
-# MaratonUSP
-Aqui ficam os arquivos com programas que resolvem questões de maratonas de programação das quais participei.
--- Resolvidos/Palavras-cruzadas/letter.cpp
+++
@@ -1,9 +0,0 @@
-#include "letter.h"
-
-Letter::Letter(int x, int y , char ch, bool inter)
-{
-    this->x = x;
-    this->y = y;
-    this->ch = ch;
-    this->inter = inter;
-}
