41321d2acad7a04e9f3d16d35b3ba94838ef40f9
APF2000 <arthurpfonseca@gmail.com>
2022-03-21 19:44:57 -0300

    [tree-painting] :heavy_check_mark: Accepted

--- Listas/OBI2020/[6]grafos-dfs/tree-painting/tree_painting.cpp
+++ Listas/OBI2020/[6]grafos-dfs/tree-painting/tree_painting.cpp
@@ -11,7 +11,9 @@ using namespace std;
 
 #define debug(x) cout << #x << " = " << x << ", ";
 
-void debug_map_map(unordered_map<long, unordered_map<long, long>> mm)
+typedef long long ll;
+
+void debug_map_map(unordered_map<ll, unordered_map<ll, ll>> mm)
 {
 	for(auto el : mm)
 	{
@@ -24,28 +26,20 @@ void debug_map_map(unordered_map<long, unordered_map<long, long>> mm)
 	}
 }
 
-long n;
-long count = 0;
-unordered_map<long, unordered_map<long, long>> scores, qttys;
-unordered_map<long, unordered_set<long>> graph;//, visited;
-vector<pair<long, long>> visited_branchs;
-unordered_map<long, long> partial_scores, partial_qttys;
-//unordered_map<long , long> fathers;
+ll n;
+ll count = 0;
+unordered_map<ll, unordered_map<ll, ll>> scores, qttys;
+unordered_map<ll, unordered_set<ll>> graph;
+unordered_map<ll, ll> partial_scores, partial_qttys;
 
-void add_score(long v1, long v2)
+void add_score(ll v1, ll v2)
 {
 	if(graph.find(v1) == graph.end())
 	{
 		graph[v1] = {};
-		//visited[v1] = {};
 	}
 
-	//debug(v1);
-	//debug(v2);
-	////cout << endl;
-
 	graph[v1].insert(v2);
-
 
 	if(scores.find(v1) == scores.end())
 	{
@@ -53,21 +47,20 @@ void add_score(long v1, long v2)
 		qttys[v1] = {};
 	}
 
-
 	scores[v1][v2] = -1;
 	qttys[v1][v2] = -1;
 }
 
-void calc_score_and_qtty(long root, long req_node)//, long *score, long *qtty)
+void calc_score_and_qtty(ll root, ll req_node)
 {
-	long new_qtty = 1;
-	long new_score = 1;
+	ll new_qtty = 1;
+	ll new_score = 1;
 
-	for(long child : graph[req_node])
+	for(ll child : graph[req_node])
 	{
-		long c_node = child;
-		long c_score = scores[req_node][c_node];
-		long c_qtty = qttys[req_node][c_node];
+		ll c_node = child;
+		ll c_score = scores[req_node][c_node];
+		ll c_qtty = qttys[req_node][c_node];
 
 		if(c_node == root) continue;
 
@@ -84,31 +77,29 @@ void calc_score_and_qtty(long root, long req_node)//, long *score, long *qtty)
 		new_qtty += c_qtty;
 		new_score += (c_score + c_qtty);
 	}
-
+	
 	qttys[root][req_node] = new_qtty;
 	scores[root][req_node] = new_score;
 	
 }
 
-void calc_remaning_score_and_qtty(long root)
+void calc_remaning_score_and_qtty(ll root)
 {
-	pair<long, long> aux_struct = {root, 0};
-	queue<pair<long, long>> next_structs;
-	unordered_set<long> visited = {root};
+	pair<ll, ll> aux_struct = {root, 0};
+	queue<pair<ll, ll>> next_structs;
+	unordered_set<ll> visited = {root};
 
 	next_structs.push(aux_struct);
 
 	while(!next_structs.empty())
 	{
-		pair<long, long> aux_struct = next_structs.front();
+		pair<ll, ll> aux_struct = next_structs.front();
 		next_structs.pop();
 
-		cout << next_structs.size() << endl;
+		ll node = aux_struct.first;
+		ll father = aux_struct.second;
 
-		long node = aux_struct.first;
-		long father = aux_struct.second;
-
-		for(long child : graph[node])
+		for(ll child : graph[node])
 		{
 			aux_struct = { child, node };
 			if(visited.find(child) == visited.end()) // not visited
@@ -120,14 +111,14 @@ void calc_remaning_score_and_qtty(long root)
 		
 		if(father == 0) continue;
 
-		long score_father = scores[0][father];
-		long score_node = partial_scores[node];
+		ll score_father = scores[0][father];
+		ll score_node = partial_scores[node];
 
-		long qtty_father = qttys[0][father];
-		long qtty_node = partial_qttys[node];
+		ll qtty_father = qttys[0][father];
+		ll qtty_node = partial_qttys[node];
 
-		long new_score = score_father - (score_node + qtty_node);
-		long new_qtty = qtty_father - qtty_node;
+		ll new_score = score_father - (score_node + qtty_node);
+		ll new_qtty = qtty_father - qtty_node;
 
 		scores[node][father] = new_score;
 		qttys[node][father] = new_qtty;
@@ -140,16 +131,16 @@ void calc_remaning_score_and_qtty(long root)
 int main()
 {
 	cin >> n;
-	for (long i = 0; i < n - 1; i++)
+	for (ll i = 0; i < n - 1; i++)
 	{
-		long v1, v2;
+		ll v1, v2;
 		cin >> v1 >> v2;		
 
 		add_score(v1, v2);
 		add_score(v2, v1);
 	}
 
-	for (long i = 1; i <= n; i++)
+	for (ll i = 1; i <= n; i++)
 	{
 		add_score(0, i);
 
@@ -159,11 +150,12 @@ int main()
 	}
 
 	calc_score_and_qtty(0, 1);
+	calc_remaning_score_and_qtty(1);
 
-	long max_score = -1;
+	ll max_score = -1;
 	for(auto el : scores[0])
 	{
-		long new_score = el.second;
+		ll new_score = el.second;
 		if(new_score > max_score) max_score = new_score;
 	}
 	cout << max_score << endl;
