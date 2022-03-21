84ebbc0d06a5907d7d6688ee2d4814396859ebd6
APF2000 <arthurpfonseca@gmail.com>
2022-03-21 19:22:31 -0300

    [tree-painting] Clear output, but wrong for ex2

--- Listas/OBI2020/[6]grafos-dfs/tree-painting/tree_painting.cpp
+++ Listas/OBI2020/[6]grafos-dfs/tree-painting/tree_painting.cpp
@@ -63,16 +63,6 @@ void calc_score_and_qtty(long root, long req_node)//, long *score, long *qtty)
 	long new_qtty = 1;
 	long new_score = 1;
 
-	visited_branchs.push_back( {root, req_node} );
-
-	// long a;
-	// cin >> a;
-
-	// debug(root);
-	// debug(req_node);
-	// cout << endl;
-
-
 	for(long child : graph[req_node])
 	{
 		long c_node = child;
@@ -81,42 +71,20 @@ void calc_score_and_qtty(long root, long req_node)//, long *score, long *qtty)
 
 		if(c_node == root) continue;
 
-		// debug(req_node);
-		// debug(c_node);
-		// debug(new_qtty);
-		// debug(new_score);
-		// cout << endl;
-
-		//debug(c_score);
-		//debug(c_qtty);
-		//cout << endl;
-
 		if(c_score == -1 || c_qtty == -1)
 		{
 			calc_score_and_qtty(req_node, c_node);
 			c_score = scores[req_node][c_node];
 			c_qtty = qttys[req_node][c_node];
 		}	
-
-		//debug(c_score);
-		//debug(c_qtty);
-		//cout << endl;
+		
 		partial_qttys[req_node] += c_qtty;
 		partial_scores[req_node] += (c_score + c_qtty);
-
-		// if(c_node != root)
-		// {
+		
 		new_qtty += c_qtty;
 		new_score += (c_score + c_qtty);
-		//}
 	}
 
-
-	//debug(root);
-	//debug(req_node);
-	//debug(new_qtty);
-	//debug(new_score);
-	//cout << endl;
 	qttys[root][req_node] = new_qtty;
 	scores[root][req_node] = new_score;
 	
@@ -135,25 +103,16 @@ void calc_remaning_score_and_qtty(long root)
 		pair<long, long> aux_struct = next_structs.front();
 		next_structs.pop();
 
-		cout << "---------------" << endl;
-
 		cout << next_structs.size() << endl;
 
 		long node = aux_struct.first;
 		long father = aux_struct.second;
-
-
-		debug(node);
-		debug(father);
-		cout << endl;
 
 		for(long child : graph[node])
 		{
 			aux_struct = { child, node };
 			if(visited.find(child) == visited.end()) // not visited
 			{
-				debug(child);
-				cout << endl;
 				next_structs.push(aux_struct);
 				visited.insert(child);
 			}
@@ -170,15 +129,6 @@ void calc_remaning_score_and_qtty(long root)
 		long new_score = score_father - (score_node + qtty_node);
 		long new_qtty = qtty_father - qtty_node;
 
-
-		debug(score_father);
-		debug(qtty_father);
-		debug(score_node);
-		debug(qtty_node);
-		debug(new_score);
-		debug(new_qtty);
-		cout << endl;
-
 		scores[node][father] = new_score;
 		qttys[node][father] = new_qtty;
 
@@ -193,8 +143,7 @@ int main()
 	for (long i = 0; i < n - 1; i++)
 	{
 		long v1, v2;
-		cin >> v1 >> v2;
-		
+		cin >> v1 >> v2;		
 
 		add_score(v1, v2);
 		add_score(v2, v1);
@@ -203,10 +152,6 @@ int main()
 	for (long i = 1; i <= n; i++)
 	{
 		add_score(0, i);
-		// add_score(i, 0);
-
-		//debug_map_map(qttys);
-
 
 		partial_scores[i] = 1;
 		partial_qttys[i] = 1;
@@ -214,33 +159,6 @@ int main()
 	}
 
 	calc_score_and_qtty(0, 1);
-	//max_score = scores[0][1];
-	debug_map_map(qttys);
-	debug_map_map(scores);
-
-	for(auto branch : visited_branchs)
-	{
-		cout << branch.first << " " << branch.second << endl;
-	}
-	cout << "==============" << endl;
-
-	for(auto qtty : partial_qttys)
-	{
-		cout << qtty.first << " " << qtty.second << endl;
-	}
-	cout << "==============" << endl;
-
-	for(auto score : partial_scores)
-	{
-		cout << score.first << " " << score.second << endl;
-	}
-	cout << "==============" << endl;
-
-	calc_remaning_score_and_qtty(1);
-	debug_map_map(qttys);
-	cout << "==============" << endl;
-	debug_map_map(scores);
-	
 
 	long max_score = -1;
 	for(auto el : scores[0])
@@ -249,7 +167,6 @@ int main()
 		if(new_score > max_score) max_score = new_score;
 	}
 	cout << max_score << endl;
-	// //cout << count << endl;
 
 	return 0;
 }
