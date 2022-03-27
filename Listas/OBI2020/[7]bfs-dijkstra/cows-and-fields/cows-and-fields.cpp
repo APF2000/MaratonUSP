c362ecd4a29111861511681cab7f36d0f53bfc82
APF2000 <arthurpfonseca@gmail.com>
2022-03-27 12:38:30 -0300

    [cows-and-fields]: Time limit exceeded

--- Listas/OBI2020/[7]bfs-dijkstra/cows-and-fields/cows-and-fields.cpp
+++ Listas/OBI2020/[7]bfs-dijkstra/cows-and-fields/cows-and-fields.cpp
@@ -35,8 +35,6 @@ ll n, m, k;
 
 vector<ll> djikstra(ll start)
 {
-	debug(start);
-	
 	vector<ll> dists(n);
 	for (int i = 0; i < n; i++) dists[i] = INF; // all nodes are infinitly far away
 	dists[start] = 0;
@@ -49,9 +47,6 @@ vector<ll> djikstra(ll start)
 
 	while (nodes_to_visit.size() > 0)
 	{
-		debug_q(nodes_to_visit);
-		debug_v(visited);
-
 		ll id_node = nodes_to_visit.front();
 		nodes_to_visit.pop();
 		visited[id_node] = true;
@@ -61,24 +56,15 @@ vector<ll> djikstra(ll start)
 			bool was_visited = visited[adj_node];
 			if(!was_visited)
 			{
-				cout << "to be visited: " << adj_node << endl;
 				nodes_to_visit.push(adj_node);
 			}						
 
 			ll old_weight = dists[adj_node];
 			ll new_weight = dists[id_node] + 1;
 
-			debug_ln(id_node);
-			debug_ln(adj_node);
-			debug_ln(old_weight);
-			debug_ln(new_weight);
-
 			if(new_weight < old_weight) dists[adj_node] = new_weight;
-			debug_v(dists);
-			cout << "--------------" << endl;
 		}
 	}
-	cout << "==============" << endl;
 
 	return dists;	
 }
@@ -103,28 +89,12 @@ int main()
 		v1--;
 		v2--;
 
-		debug(v1);
-		debug_ln(v2);
-
 		graph[v1].insert(v2);
 		graph[v2].insert(v1);
 	}
 
-	for (int i = 0; i < n; i++)
-	{
-		cout << i << " : ";
-		for(ll el : graph[i])
-		{
-			cout << el << " ";
-		}
-		cout << endl;
-	}
-
 	vector<ll> dists_start 	= djikstra(0);
 	vector<ll> dists_end 	= djikstra(n - 1);
-
-	debug_v(dists_start);
-	debug_v(dists_end);
 
 	vector<pair<ll, ll>> dists_diff;
 	for (int i = 0; i < n; i++)
@@ -132,41 +102,25 @@ int main()
 		bool is_special = (specials.find(i) != specials.end());
 		if(is_special)
 		{
-			ll diff = dists_end[i] - dists_start[i];
+			ll diff = dists_start[i] - dists_end[i];
 			dists_diff.push_back( {diff, i} );
 		}
 	}
 
-	for(auto el : dists_diff)
-	{
-		cout << el.first << " ";
-	}
-	cout << endl;
 	sort(dists_diff.begin(), dists_diff.end());
 
-	for(auto el : dists_diff)
+	ll best = 0;
+	ll max_dist = -INF;
+	for(auto it : dists_diff)
 	{
-		cout << el.first << " ";
+		ll id_node = it.second;
+		best = max(best, max_dist + dists_end[id_node]);
+		max_dist = max(max_dist, dists_start[id_node]);
 	}
-	cout << endl;
 
-	ll size = dists_diff.size();
-	ll id_node1 = dists_diff[0].second;
-	ll id_node2 = dists_diff[1].second;
-
-	cout << id_node1 << " " << id_node2 << endl;
-
-	// compare dists when node1 and node2 are connected
-	ll dist1 = dists_start[id_node1] + dists_end[id_node2] + 1;
-	ll dist2 = dists_start[id_node2] + dists_end[id_node1] + 1;
 	ll dist3 = dists_start[n - 1];
-
-	debug_ln(dist1);
-	debug_ln(dist2);
-	debug_ln(dist3);
-
-	ll min_dist = min({dist1, dist2, dist3});
-	debug_ln(min_dist);
+	ll min_dist = min({best + 1, dist3});
+	cout << min_dist << endl;
 
 	return 0;
 }
