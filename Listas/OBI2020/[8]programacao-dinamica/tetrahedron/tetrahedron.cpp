562f734b76f83f608802b8fd5435ffabf1f71415
APF2000 <arthurpfonseca@gmail.com>
2022-05-18 19:17:33 +0200

    [tetrahedron]: :heavy_check_mark: Accepted: use unsigned long instead

--- Listas/OBI2020/[8]programacao-dinamica/tetrahedron/tetrahedron.cpp
+++ Listas/OBI2020/[8]programacao-dinamica/tetrahedron/tetrahedron.cpp
@@ -6,34 +6,32 @@
 #include <vector>
 
 #define MOD 1000000007
-#define BLANK -1
 
 using namespace std;
-
-typedef long long ll;
 
 #define debug(VAR) cout << #VAR << " = " << VAR << ", ";
 #define debug_ln(VAR) cout << #VAR << " = " << VAR << endl;
 #define debug_v(v) cout << #v << " = "; for(auto el : v) cout << el << ", "; cout << endl;
 
+typedef unsigned long ul;
 
-ll calc_moves(ll n)
+ul calc_moves(ul n)
 {
-	vector<ll> paths_from_d, paths_from_not_d;
+	vector<ul> paths_from_d, paths_from_not_d;
 
-	for (ll i = 0; i < n; i++)
+	for (long i = 0; i < n; i++)
 	{
-		paths_from_d.push_back(BLANK);
-		paths_from_not_d.push_back(BLANK);
+		paths_from_d.push_back(0);
+		paths_from_not_d.push_back(0);
 	}
 
 	paths_from_d[0] = 0;
 	paths_from_not_d[0] = 1;
 
-	for (ll i = 1; i < n; i++)
+	for (ul i = 1; i < n; i++)
 	{
-		ll qtty_not_d = paths_from_not_d[i - 1];
-		ll qtty_d = paths_from_d[i - 1];
+		ul qtty_not_d = paths_from_not_d[i - 1];
+		ul qtty_d = paths_from_d[i - 1];
 
 		paths_from_d[i] 	= (3 * qtty_not_d) % MOD;
 		paths_from_not_d[i] = (qtty_d + 2 * qtty_not_d) % MOD;
@@ -44,10 +42,10 @@ ll calc_moves(ll n)
 
 int main()
 {
-	ll n;
+	ul n;
 	cin >> n;
 
-	ll result = calc_moves(n);
+	ul result = calc_moves(n);
 	cout << result << endl;
 
 	return 0;
