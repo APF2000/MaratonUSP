1c403d504b6f15314ab9718cb7a3948d249dffff
APF2000 <arthurpfonseca@gmail.com>
2022-05-18 18:08:52 +0200

    [tetrahedron]: Initialize with template

---
+++ Listas/OBI2020/[8]programacao-dinamica/tetrahedron/tetrahedron.cpp
@@ -0,0 +1,28 @@
+// https://codeforces.com/problemset/problem/166/E
+
+// #include <bits/stdc++.h>
+
+#include <iostream>
+
+using namespace std;
+
+#define debug(VAR) cout << #VAR << " = " << VAR << ", ";
+#define debug_ln(VAR) cout << #VAR << " = " << VAR << endl;
+#define debug_v(v) cout << #v << " = "; for(auto el : v) cout << el << ", "; cout << endl;
+
+void debug_q(queue<ll> q)
+{
+	while(q.size() != 0)
+	{
+		int el = q.front();
+		q.pop();
+
+		cout << el << ", ";
+	}
+	cout << endl;
+}
+
+int main()
+{
+	return 0;
+}
