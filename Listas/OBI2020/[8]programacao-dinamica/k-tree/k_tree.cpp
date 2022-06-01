6da26df57de1fcf4a73d49a9c49c92c810e0cff6
APF2000 <arthurpfonseca@gmail.com>
2022-06-01 18:44:07 +0200

    [k-tree]: One more seg fault

--- Listas/OBI2020/[8]programacao-dinamica/k-tree/k_tree.cpp
+++ Listas/OBI2020/[8]programacao-dinamica/k-tree/k_tree.cpp
@@ -18,33 +18,73 @@ typedef long long ll;
 #define debug_v(v) cout << #v << " = "; for(auto el : v) cout << el << ", "; cout << endl;
 #define debug_m(m) cout << #m << " = "; for(auto v : m) { for(auto el : v) { cout << el << ", "; } cout << endl; } cout << endl;
 
-vector<ll> mem_paths_with_d, mem_paths_without_d;
+vector<vector<ll>> mem_paths_with_d, mem_paths_without_d;
 
 void calc_paths(int n, int k, int d, ll *paths_with_d, ll *paths_without_d)
 {
-	//ll aux_with_d = 1, aux_without_d = k - 1;
-
-	// for (int i = 0; i < n; i++)
-	// {
-	// 	*paths_without_d *= (k - 1);
-	// 	*paths_with_d = () - *paths_without_d;
-
-	// 	debug_ln(*paths_with_d);
-	// 	debug_ln(*paths_without_d);
-	// }
-	ll mult_d = 1, mult_not_d = 1;
-	for (int i = 0; i < n; i++)
+	if(n <= 0) 
 	{
-		mult_d *= (k - 1);
-		mult_not_d *= (k);
+		cout << "n: " << n << " <= 0" << endl;
 
-		debug_ln(mult_d);
-		debug_ln(mult_not_d);
+		*paths_with_d = 0;
+		*paths_without_d = 0;
+		return;
 	}
 
-	*paths_without_d = mult_d;
-	*paths_with_d = mult_not_d - mult_d;
-	
+	// debug_ln(n);
+	// debug_ln(k);
+	// debug_ln(min(k, n - k));
+
+	for (int i = 1; i <= min(n, k); i++)
+	{
+		cout << "=================" << endl;
+		debug_ln(i);
+		debug_ln(n);
+		debug_ln(n - i);
+
+		ll (*aux_paths_with_d), (*aux_paths_without_d) ;
+		(*aux_paths_with_d) = 0;
+		(*aux_paths_without_d) = 1;
+
+		// count for child subtree
+		if(mem_paths_with_d[n - i][i] == BLANK) // works for any of the mems
+		{
+			cout << "-----antes calc" << endl;
+			debug_ln(*aux_paths_with_d);
+			debug_ln(*aux_paths_without_d);
+			calc_paths(n - i, k, d, aux_paths_with_d, aux_paths_without_d);
+			//debug_m(mem_paths_with_d);
+			cout << "-----depois calc----" << endl;
+			debug_ln(*aux_paths_with_d);
+			debug_ln(*aux_paths_without_d);
+		debug_ln(i);
+		debug_ln(n);
+		debug_ln(n - i);
+
+			mem_paths_with_d[n - i][i] 		= *aux_paths_with_d;
+			mem_paths_without_d[n - i][i]  	= *aux_paths_without_d;
+		}
+
+		// sum child subtree to current subtree
+		if(i == d) 
+		{
+			cout << "i == d" << endl;
+			debug_ln(aux_paths_with_d);
+			debug_ln(aux_paths_without_d);
+			debug_ln(*aux_paths_with_d + *aux_paths_without_d);
+			debug_ln((*paths_with_d));
+
+			(*paths_with_d) += *aux_paths_with_d + *aux_paths_without_d;
+			//*paths_without_d += 0;
+		}
+
+			debug_ln((*paths_with_d));
+			debug_ln((*paths_without_d));
+
+	debug_m(mem_paths_with_d);	
+	debug_m(mem_paths_without_d);	
+		//debug_ln(i);
+	}
 }
 
 int main()
@@ -53,34 +93,25 @@ int main()
 
 	cin >> n >> k >> d;
 	
-	for (int i = 0; i < k; i++)
+	for (int i = 0; i < n; i++)
 	{
-		// vector<ll> aux_v;
-		// for (int j = 0; j < k; j++)
-		// {
-		// 	aux_v.push_back(BLANK);
-		// }
-		// mem_paths_with_d.push_back(aux_v);
-		// mem_paths_without_d.push_back(aux_v);
-		mem_paths_with_d.push_back(BLANK);
-		mem_paths_without_d.push_back(BLANK);
+		vector<ll> aux_v;
+		for (int j = 0; j < n; j++)
+		{
+			aux_v.push_back(BLANK);
+		}
+		mem_paths_with_d.push_back(aux_v);
+		mem_paths_without_d.push_back(aux_v);
 	}
 
-	// debug_m(mem_paths_with_d);	
-	// debug_m(mem_paths_without_d);	
-	// debug_v(mem_paths_with_d);	
-	// debug_v(mem_paths_without_d);	
+	debug_m(mem_paths_with_d);	
+	debug_m(mem_paths_without_d);	
 
-	ll paths_with_d = 0, paths_without_d = 1;
+	ll paths_with_d, paths_without_d;
 	calc_paths(n, k, d, &paths_with_d, &paths_without_d);
 
-	debug_ln(paths_with_d);
-	debug_ln(paths_without_d);
-
-	// debug_m(mem_paths_with_d);	
-	// debug_m(mem_paths_without_d);	
-	// debug_v(mem_paths_with_d);	
-	// debug_v(mem_paths_without_d);	
+	debug_m(mem_paths_with_d);	
+	debug_m(mem_paths_without_d);	
 
 	return 0;
 }
