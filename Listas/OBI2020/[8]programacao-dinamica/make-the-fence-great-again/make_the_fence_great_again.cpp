63f534a260db101e31890f071c1b06bc0bda5c88
APF2000 <arthurpfonseca@gmail.com>
2022-06-08 19:13:26 +0200

    [make-the-fence-great-again]: Initial ideas

---
+++ Listas/OBI2020/[8]programacao-dinamica/make-the-fence-great-again/make_the_fence_great_again.cpp
@@ -0,0 +1,42 @@
+// https://codeforces.com/contest/1221/problem/D
+
+// #include <bits/stdc++.h>
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define MODULO 1000000007
+#define BLANK -1
+
+using namespace std;
+
+typedef long long ll;
+
+#define debug(VAR) cout << #VAR << " = " << VAR << ", ";
+#define debug_ln(VAR) cout << #VAR << " = " << VAR << endl;
+#define debug_v(v) cout << #v << " = "; for(auto el : v) cout << el << ", "; cout << endl;
+#define debug_m(m) cout << #m << " = "; for(auto v : m) { for(auto el : v) { cout << el << ", "; } cout << endl; } cout << endl;
+
+
+int main()
+{
+	long q;
+	cin >> q;
+
+	for (long i = 0; i < q; i++)
+	{
+		long n;
+		cin >> n;
+
+		for (long j = 0; j < n; j++)
+		{
+			long a, b;
+			cin >> a >> b;
+		}
+		
+	}
+	
+
+	return 0;
+}
