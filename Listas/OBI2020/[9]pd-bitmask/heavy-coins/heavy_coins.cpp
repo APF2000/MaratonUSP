99b61ca508054b0a361c243e76d307465b90acb1
APF2000 <arthurpfonseca@gmail.com>
2022-07-01 16:36:43 +0200

    [heavy-coins]: New approach: bitmask

--- Listas/OBI2020/[9]pd-bitmask/heavy-coins/heavy_coins.cpp
+++ Listas/OBI2020/[9]pd-bitmask/heavy-coins/heavy_coins.cpp
@@ -19,64 +19,97 @@ using namespace std;
 int max_coins(vector<int>coins, int debt, int n, 
     unordered_set<int> used_coins, vector<vector<int>> &mem)
 {
+    //dln((1 << n));
+    int result = 0;
+
+    for (int i = 0; i < (1 << n); i++)
+    {
+        int value_sum = 0, lowest_coin = 0, qtty_sum = 0;
+
+        for (int aux_num = i, j = n - 1; aux_num > 0; aux_num /= 2, j--)
+        {
+            bool should_use_coin = (aux_num % 2 == 1);
+            if(should_use_coin)
+            {
+                int this_coin = coins[j];
+                lowest_coin = this_coin;
+                value_sum += this_coin;
+                qtty_sum += 1;
+            }
+        }
+
+        bool is_max_coins_case = qtty_sum > result;
+        bool is_exact_debt = value_sum == debt;
+        bool cant_remove_excess = (value_sum > debt && value_sum - lowest_coin >= debt);
+
+        if( is_max_coins_case && ( is_exact_debt || cant_remove_excess ) )
+            result = qtty_sum;
+
+        // if(!(qtty_sum < result || value_sum < debt))
+        // ) result = qtty_sum;
+    }
+
+    return result;
+    
+
     // d(debt);
     // d(n);
     // dv(used_coins);
     //dm(mem);
 
-    if(used_coins.size() == n) return 0;
-    if(debt == 0) return 0;
-    if(debt < 0)
-    {
-        for(int coind_id : used_coins)
-        {
-            int value = coins[coind_id];
-            if(value <= -debt) return INVALID; 
-        }      
+    // if(used_coins.size() == n) return 0;
+    // if(debt == 0) return 0;
+    // if(debt < 0)
+    // {
+    //     for(int coind_id : used_coins)
+    //     {
+    //         int value = coins[coind_id];
+    //         if(value <= -debt) return INVALID; 
+    //     }      
 
-        return 0;
-    }
+    //     return 0;
+    // }
 
-    int max_coins_qtty = 0;
+    // int max_coins_qtty = 0;
 
-    for (int i = n - 1; i >= 0; i--)
-    {
-        bool was_used = (used_coins.find(i) != used_coins.end());
-        if(!was_used)
-        {
-            int value = coins[i];
-            int new_debt = debt - value;
+    // for (int i = n - 1; i >= 0; i--)
+    // {
+    //     bool was_used = (used_coins.find(i) != used_coins.end());
+    //     if(!was_used)
+    //     {
+    //         int value = coins[i];
+    //         int new_debt = debt - value;
             
-            // dln(value);
-            // dln(debt);
-            // dln(i);
-            //dm(mem);
+    //         // dln(value);
+    //         // dln(debt);
+    //         // dln(i);
+    //         //dm(mem);
 
-            if(new_debt < 0) 
-            {
-                max_coins_qtty = 0;
-                break;
-            }
+    //         if(new_debt < 0) 
+    //         {
+    //             max_coins_qtty = 0;
+    //             break;
+    //         }
             
-            int coins_qtty = mem[new_debt][i];
+    //         int coins_qtty = mem[new_debt][i];
 
-            if(coins_qtty == BLANK)
-            {
-                unordered_set<int> aux_used_coins = used_coins;
-                aux_used_coins.insert(i);
+    //         if(coins_qtty == BLANK)
+    //         {
+    //             unordered_set<int> aux_used_coins = used_coins;
+    //             aux_used_coins.insert(i);
 
-                coins_qtty = max_coins(coins, debt - value, n, aux_used_coins, mem);
-                if(coins_qtty == INVALID) continue;
+    //             coins_qtty = max_coins(coins, debt - value, n, aux_used_coins, mem);
+    //             if(coins_qtty == INVALID) continue;
 
-                mem[debt - value][i] = coins_qtty;
-            }
+    //             mem[debt - value][i] = coins_qtty;
+    //         }
 
-            max_coins_qtty = max(max_coins_qtty, coins_qtty);
-        }
-    }    
+    //         max_coins_qtty = max(max_coins_qtty, coins_qtty);
+    //     }
+    // }    
     
 
-    return max_coins_qtty + 1;
+    // return max_coins_qtty + 1;
 }
 
 int main()
@@ -112,7 +145,7 @@ int main()
         sort(coins.begin(), coins.end());
 
         int result = max_coins(coins, debt, n, {}, mem);
-        dm(mem);
+        //dm(mem);
 
         cout << result << endl;
     }
