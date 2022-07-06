2e03281757c51212ceb3a1a3659679f6b846ce0c
APF2000 <arthurpfonseca@gmail.com>
2022-07-06 19:08:32 +0200

    [produto-do-intervalo]: Init problem

---
+++ Listas/OBI2020/[10]segtree/produto-do-intervalo/produto_do_intervalo.cpp
@@ -0,0 +1,33 @@
+// https://neps.academy/br/exercise/327
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define BLANK 1e9
+#define INVALID -1
+
+#define d(VAR) cout << #VAR << " = " << VAR << ", ";
+#define dln(VAR) cout << #VAR << " = " << VAR << endl;
+#define dv(v) cout << #v << " = "; for(auto el : v) cout << el << ", "; cout << endl;
+#define dm(m) cout << #m << " = "; for(auto v : m) { for(auto el : v) cout << el << ", ";  cout << endl; }
+
+int max_coins(vector<int>coins, int debt, int n)
+{
+
+    return result;
+}
+
+int main()
+{
+    int t;
+
+    cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+    }
+    
+
+	return 0;
+}
