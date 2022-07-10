60cd2e8e3a102f98076b4c08507fde669f5ed2c3
APF2000 <arthurpfonseca@gmail.com>
2022-07-10 14:53:29 +0200

    [produto-do-intervalo]: Copying basic segtree code

--- Listas/OBI2020/[10]segtree/produto-do-intervalo/produto_do_intervalo.cpp
+++ Listas/OBI2020/[10]segtree/produto-do-intervalo/produto_do_intervalo.cpp
@@ -4,20 +4,53 @@
 #include <vector>
 using namespace std;
 
-#define BLANK 1e9
-#define INVALID -1
+#define CHANGE 'C'
+#define PROD 'P'
 
 #define d(VAR) cout << #VAR << " = " << VAR << ", ";
 #define dln(VAR) cout << #VAR << " = " << VAR << endl;
 #define dv(v) cout << #v << " = "; for(auto el : v) cout << el << ", "; cout << endl;
 #define dm(m) cout << #m << " = "; for(auto v : m) { for(auto el : v) cout << el << ", ";  cout << endl; }
 
-vector<int>coins
+
+vector<int> values, tree;
+
+void build(int id, int l, int r)
+{
+    if(l == r)
+    {
+        tree[id] = values[l];
+    }
+    else
+    {
+        int m = (l + r) / 2;
+        build(2 * id, l, m);
+        build(2 * id + 1, m + 1, r);
+        tree[id] = tree[2 * id] * tree[2 * id + 1];    
+    }
+}
+
+void update(int id, int l, int r, int ql, int qr, int value)
+{
+    if(ql <= l && r <= qr)
+    {
+        tree[id] = value; // verificar
+        return;
+    }
+
+    if(qr < l || ql > r) return;
+
+    int m = (l + r) / 2;
+    update(2 * id, l, m, ql, qr, value);
+    update(2 * id + 1, m, r, ql, qr, value);
+}
 
 int query(int id, int l, int r, int ql, int qr)
 {
-    if(ql <= l && qr >= r) return [id];
-    if(qr < l || r < ql) return 0;
+    if(id >= values.size()) return 1;
+
+    if(ql <= l && qr >= r) return values[id];
+    if(qr < l || r < ql) return 1;
 
     int m = (l + r) / 2; 
 
@@ -27,14 +60,61 @@ int query(int id, int l, int r, int ql, int qr)
 
 int main()
 {
-    int t;
+    long n, k;
 
-    cin >> t;
-
-    for (int i = 0; i < t; i++)
+    while(cin >> n >> k)
     {
-    }
-    
+        values = vector<int>(n + 1, 1);
+        tree = vector<int>(4 * (n + 1), 1);
+        long prod = 1;
+
+        for (long i = 0; i < n; i++)
+        {
+            // int aux;
+            // cin >> aux;
+            cin >> values[i];
+            //values.push_back(aux);
+        }
+
+        //dv(tree);
+        dv(values);
+        build(0, 0, n - 1);
+        dv(tree);
+        
+        for (long i = 0; i < k; i++)
+        {
+
+            char cmd;
+            cin >> cmd;
+            if(cmd == CHANGE)
+            {
+                long id;
+                int value;
+
+                cin >> id >> value;
+
+                update(id, 1, n - 1, 1, n - 1, value);
+
+                //dv(tree);
+
+                //values[id - 1] = value;
+            
+                //dv(values);
+
+                // cout << query(0, 1, n - 1, 1, 3) << endl;
+                // cout << query(0, 1, n - 1, 0, values.size()-1) << endl;
+                // cout << query(0, 1, n - 1, 0, 2) << endl;
+            }
+            else if(cmd == PROD)
+            {
+                long id;
+                int value;
+
+                cin >> id >> value;
+            }
+        }
+        
+    } 
 
 	return 0;
 }
