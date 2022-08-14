89ffbeabf993670be8d331bdb90ea24773ead91f
APF2000 <arthurpfonseca@gmail.com>
2022-08-14 14:48:52 +0200

    [fast-flow]: Edmonds-Karp: first try

--- Listas/Desafios2_2021/fast-flow/fast_flow.cpp
+++ Listas/Desafios2_2021/fast-flow/fast_flow.cpp
@@ -8,6 +8,7 @@
 
 typedef unsigned long long ull;
 #define BLANK -1
+#define INF 10e9
 
 using namespace std;
 
@@ -27,7 +28,7 @@ void dmap(unordered_map<int, unordered_set<int>> m)
 }
 
 int n, m;
-vector<vector<ull>> weights;
+vector<vector<ull>> capacities;
 unordered_map<int, unordered_set<int>> u_graph, d_graph; 
 
 void build_u_graph()
@@ -42,11 +43,11 @@ void build_u_graph()
 
 		if(a == b) continue;
 
-		ull new_w = c, old_w = weights[a][b];
+		ull new_w = c, old_w = capacities[a][b];
 		if(old_w != BLANK) new_w += old_w;
 
-		weights[a][b] = new_w;
-		weights[b][a] = new_w;
+		capacities[a][b] = new_w;
+		capacities[b][a] = new_w;
 
 		// add to undirected graph
 		u_graph[a].insert(b);
@@ -91,19 +92,135 @@ void build_d_graph()
 	}
 }
 
+ull edmonds_karp()
+{
+	// prepare residual graph
+	unordered_map<int, unordered_set<int>> r_graph_normal = d_graph, r_graph_reverse;
+	vector<vector<ull>> flows = capacities;
+
+	for(pair<int, unordered_set<int>> kv : d_graph)
+	{
+		unordered_set<int> adj_ids = kv.second;
+		int node_id = kv.first;
+
+		for(int adj_id : adj_ids)
+		{
+			flows[adj_id][node_id] = 0;
+			r_graph_reverse[adj_id].insert(node_id);
+		}
+	}
+
+
+	// while (true)
+		// find shortest s-t-path
+		unordered_set<int> adjs = d_graph[0];
+		unordered_set<int> visited = {};
+
+		queue<int> nodes_to_visit, level_nodes_to_visit;
+		nodes_to_visit.push(0);
+		level_nodes_to_visit.push(0);
+
+		int last_level = 0;
+		vector<int> s_t_path = {0};
+
+		while(!nodes_to_visit.empty())
+		{
+			int node = nodes_to_visit.front();
+			nodes_to_visit.pop();
+			visited.insert(node);
+
+			int this_level = level_nodes_to_visit.front();
+			level_nodes_to_visit.pop();
+
+			if(last_level == this_level + 1)
+			{
+				s_t_path.push_back(node);
+				if(node == n - 1) break;
+			}
+			else if(last_level < this_level + 1)
+			{
+				dln("ALGO DE MUITO ERRADO NAO ESTA CERTO");
+			}
+
+			d(node);
+			dv(visited);
+
+			for(int adj_node : u_graph[node])
+			{
+				if(visited.find(adj_node) != visited.end()) 
+					continue; 
+
+				if(flows[node][adj_node] == 0) continue;
+
+				nodes_to_visit.push(adj_node);
+				level_nodes_to_visit.push(this_level + 1);
+
+				//cout << "a" << endl;
+				d(node);
+				dln(adj_node);
+
+				// arrow goes from node to adj_node
+				//d_graph[node].insert(adj_node);
+			}
+		}
+
+		// find out bottleneck capacity
+		ull bottleneck = INF;
+		for(int i = 1; i < s_t_path.size(); i++)
+		{
+			int node1 = s_t_path[i - 1];
+			int node2 = s_t_path[i];
+
+			ull new_flow = flows[node1][node2];
+			if(new_flow < bottleneck)
+			{
+				bottleneck = new_flow;
+			}
+		}
+
+		// change residual flows
+		for(int i = 1; i < s_t_path.size(); i++)
+		{
+			int node1 = s_t_path[i - 1];
+			int node2 = s_t_path[i];
+
+			unordered_set<int> edge_normal = r_graph_normal[node1];
+
+			// possible pointer exception
+			bool edge_is_normal = ( edge_normal.find(node2) != edge_normal.end() );
+			
+			// change flow direction symmetrically
+			if(edge_is_normal)
+			{
+				flows[node1][node2] -= bottleneck;
+				flows[node2][node1] += bottleneck;
+			}
+			else
+			{
+				flows[node1][node2] += bottleneck;
+				flows[node2][node1] -= bottleneck;
+			}
+		}
+	
+	dm(flows);
+}
+
 int main()
 {
 	cin >> n >> m;
 
-	// itialize weights with blank
-	weights = vector<vector<ull>>(n, vector<ull>(n, BLANK));
+	// itialize capacities with blank
+	capacities = vector<vector<ull>>(n, vector<ull>(n, BLANK));
 
 	build_u_graph();
 	build_d_graph();
 
-	dm(weights);
+	dm(capacities);
 	dmap(u_graph);
+	dln(d_graph.size());
 	dmap(d_graph);
+
+	edmonds_karp();
 
 	return 0;
 }
